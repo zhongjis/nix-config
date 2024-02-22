f73cdde00bc20defbeeb19dd512d3322d3409a98
zshen <zhongjie.x.shen@gmail.com>
2024-02-21 21:23:47 -0700

    enable opengl

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -72,6 +72,8 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
+  hardware.opengl.enable = true;
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.zshen = {
     isNormalUser = true;
