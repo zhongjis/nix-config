9cd206bfb8aa9f2f5f5d8055dc9f1d5c9a6847d1
zshen <zhongjie.x.shen@gmail.com>
2024-03-09 01:19:30 -0700

    update

--- modules/home-manager/waybar.nix
+++ modules/home-manager/waybar.nix
@@ -1,172 +1,169 @@
-{ pkgs, ... }:
+{ 
+    pkgs, 
+    ... 
+}:let 
+    mainBarConfig = {
+        layer = "top";
+        position = "top";
+        height = 32;
+        spacing = 4;
+        modules-left = [ 
+            "wlr/taskbar" 
+            "hyprland/window"
+        ];
+        modules-center = [
+            "hyprland/workspaces"
+        ];
+        modules-right = [
+            "idle_inhibitor"
+            "pulseaudio"
+            "bluetooth"
+            "cpu"
+            "memory"
+            "disk"
+            "temperature"
+            "network"
+            "battery"
+            "clock"
+        ];
 
-{
-  programs.waybar = {
-    enable = true;
-    settings = {
-        mainBar = {
-            layer = "top";
-            position = "top";
-            height = 32;
-            spacing = 4;
-            modules-left = [ 
-                "wlr/taskbar" 
-                "hyprland/window"
-            ];
-            modules-center = [
-              "hyprland/workspaces"
-            ];
-            modules-right = [
-                "idle_inhibitor"
-                "pulseaudio"
-                "bluetooth"
-                "cpu"
-                "memory"
-                "disk"
-                "temperature"
-                "network"
-                "battery"
-                "clock"
-            ];
-
-            "hyprland/workspaces" = {
-                on-click = "activate";
-                active-only = false;
-                all-outputs = true;
-                format = "{}";
-                format-icons = {
-                    urgent = "";
-                    active = "";
-                    default = "";
-                };
-                persistent-workspaces = {
-                    "*" = 5;
-                };
+        "hyprland/workspaces" = {
+            on-click = "activate";
+            active-only = false;
+            all-outputs = true;
+            format = "{}";
+            format-icons = {
+                urgent = "";
+                active = "";
+                default = "";
             };
-
-            "wlr/taskbar" = {
-                format = "{icon}";
-                icon-size = 18;
-                tooltip-format = "{title}";
-                on-click = "activate";
-                on-click-middle = "close";
-                ignore-list = [];
-                app_ids-mapping = {};
-                rewirte = {
-                    "Firefox Web Browser" = "Firefox";
-                };
+            persistent-workspaces = {
+                "*" = 5;
             };
+        };
 
-            "hyprland/window" = {
-                rewrite = {
-                    "(.*) - Brave" = "$1";
-                    "(.*) - Chromium" = "$1";
-                    "(.*) - Brave Search" = "$1";
-                    "(.*) - Outlook" = "$1";
-                    "(.*) Microsoft Teams" = "$1";
-                };
-                separate-outputs = true;
+        "wlr/taskbar" = {
+            format = "{icon}";
+            icon-size = 18;
+            tooltip-format = "{title}";
+            on-click = "activate";
+            on-click-middle = "close";
+            ignore-list = [];
+            app_ids-mapping = {};
+            rewirte = {
+                "Firefox Web Browser" = "Firefox";
             };
+        };
 
-            "idle_inhibitor" = {
-                format = "{icon}";
-                format-icons = {
-                    activated = "";
-                    deactivated = "";
-                };
+        "hyprland/window" = {
+            rewrite = {
+                "(.*) - Brave" = "$1";
+                "(.*) - Chromium" = "$1";
+                "(.*) - Brave Search" = "$1";
+                "(.*) - Outlook" = "$1";
+                "(.*) Microsoft Teams" = "$1";
             };
+            separate-outputs = true;
+        };
 
-            "clock"= {
-                # "timezone"= "America/New_York";
-                tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
-                format-alt = "{:%Y-%m-%d}";
+        "idle_inhibitor" = {
+            format = "{icon}";
+            format-icons = {
+                activated = "";
+                deactivated = "";
             };
+        };
 
-            "cpu"= {
-                format = "{usage}% ";
-                tooltip = false;
-            };
+        "clock"= {
+            # "timezone"= "America/New_York";
+            tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+            format-alt = "{:%Y-%m-%d}";
+        };
 
-            "memory"= {
-                format = "{}% ";
-            };
+        "cpu"= {
+            format = "{usage}% ";
+            tooltip = false;
+        };
 
-            "temperature"= {
-                # "thermal-zone"= 2;
-                # "hwmon-path"= "/sys/class/hwmon/hwmon2/temp1_input";
-                critical-threshold = 80;
-                # "format-critical"= "{temperatureC}°C {icon}";
-                format= "{temperatureC}°C {icon}";
-                format-icons = ["" "" ""];
-            };
+        "memory"= {
+            format = "{}% ";
+        };
 
-            "backlight"= {
-                # "device"= "acpi_video1";
-                format = "{percent}% {icon}";
-                format-icons = ["" "" "" "" "" "" "" "" ""];
-            };
+        "temperature"= {
+            # "thermal-zone"= 2;
+            # "hwmon-path"= "/sys/class/hwmon/hwmon2/temp1_input";
+            critical-threshold = 80;
+            # "format-critical"= "{temperatureC}°C {icon}";
+            format= "{temperatureC}°C {icon}";
+            format-icons = ["" "" ""];
+        };
 
-            "battery"= {
-                states= {
-                    # "good"= 95;
-                    warning = 30;
-                    critical = 15;
-                };
-                format = "{capacity}% {icon}";
-                format-full = "{capacity}% {icon}";
-                format-charging = "{capacity}% ";
-                format-plugged = "{capacity}% ";
-                format-alt = "{time} {icon}";
-                # format-good = ""; # An empty format will hide the module
-                # format-full = "";
-                format-icons= ["" "" "" "" ""];
-            };
+        "backlight"= {
+            # "device"= "acpi_video1";
+            format = "{percent}% {icon}";
+            format-icons = ["" "" "" "" "" "" "" "" ""];
+        };
 
-            "network" = {
-                format = "{ifname}";
-                format-wifi = "   {signalStrength}%";
-                format-ethernet = "  {ifname}";
-                format-disconnected = "Disconnected";
-                tooltip-format = " {ifname} via {gwaddri}";
-                tooltip-format-wifi = "  {ifname} @ {essid}\nIP: {ipaddr}\nStrength: {signalStrength}%\nFreq: {frequency}MHz\nUp: {bandwidthUpBits} Down: {bandwidthDownBits}";
-                tooltip-format-ethernet = " {ifname}\nIP: {ipaddr}\n up: {bandwidthUpBits} down: {bandwidthDownBits}";
-                tooltip-format-disconnected = "Disconnected";
-                max-length = 50;
-                # on-click = "~/dotfiles/.settings/networkmanager.sh";
+        "battery"= {
+            states= {
+                # "good"= 95;
+                warning = 30;
+                critical = 15;
             };
+            format = "{capacity}% {icon}";
+            format-full = "{capacity}% {icon}";
+            format-charging = "{capacity}% ";
+            format-plugged = "{capacity}% ";
+            format-alt = "{time} {icon}";
+            # format-good = ""; # An empty format will hide the module
+            # format-full = "";
+            format-icons= ["" "" "" "" ""];
+        };
 
-            "pulseaudio" = {
-                scroll-step = 1;
-                format = "{icon} {volume}%";
-                format-bluetooth = "{volume}% {icon} {format_source}";
-                format-bluetooth-muted = " {icon} {format_source}";
-                format-muted = " {format_source}";
-                format-source = "{volume}% ";
-                format-source-muted = "";
-                format-icons = {
-                    headphone = "";
-                    hands-free = "";
-                    headset = "";
-                    phone = "";
-                    portable = "";
-                    car = "";
-                    default = ["" " " " "];
-                };
-                "on-click" = "pavucontrol";
-            };
+        "network" = {
+            format = "{ifname}";
+            format-wifi = "   {signalStrength}%";
+            format-ethernet = "  {ifname}";
+            format-disconnected = "Disconnected";
+            tooltip-format = " {ifname} via {gwaddri}";
+            tooltip-format-wifi = "  {ifname} @ {essid}\nIP: {ipaddr}\nStrength: {signalStrength}%\nFreq: {frequency}MHz\nUp: {bandwidthUpBits} Down: {bandwidthDownBits}";
+            tooltip-format-ethernet = " {ifname}\nIP: {ipaddr}\n up: {bandwidthUpBits} down: {bandwidthDownBits}";
+            tooltip-format-disconnected = "Disconnected";
+            max-length = 50;
+            # on-click = "~/dotfiles/.settings/networkmanager.sh";
+        };
 
-            "bluetooth" = {
-                format = " {status}";
-                format-disabled = "";
-                format-off = "";
-                interval = 30;
-                on-click = "blueman-manager";
-                format-no-controller = "";
+        "pulseaudio" = {
+            scroll-step = 1;
+            format = "{icon} {volume}%";
+            format-bluetooth = "{volume}% {icon} {format_source}";
+            format-bluetooth-muted = " {icon} {format_source}";
+            format-muted = " {format_source}";
+            format-source = "{volume}% ";
+            format-source-muted = "";
+            format-icons = {
+                headphone = "";
+                hands-free = "";
+                headset = "";
+                phone = "";
+                portable = "";
+                car = "";
+                default = ["" " " " "];
             };
+            "on-click" = "pavucontrol";
+        };
+
+        "bluetooth" = {
+            format = " {status}";
+            format-disabled = "";
+            format-off = "";
+            interval = 30;
+            on-click = "blueman-manager";
+            format-no-controller = "";
         };
     };
 
-    style = ''
+    css = ''
         @define-color backgroundlight @color11;
         @define-color workspacesbackground1 @color11;
         @define-color bordercolor #FFFFFF;
@@ -466,6 +463,14 @@
             -gtk-icon-effect: highlight;
         }
     '';
+in {
+  programs.waybar = {
+    enable = true;
+    package = pkgs.waybar.overrideAttrs (oldAttrs: {
+      mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
+    });
+    settings.mainBar = mainBarConfig;
+    style = css;
   };
 
   home.packages = with pkgs; [
