6c91f1b3016a5fd2b7e1cb0292d789404257bc97
zshen <zhongjie.x.shen@gmail.com>
2024-03-21 23:39:12 -0600

    update zsh config

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -3,16 +3,30 @@
 {
   programs.zsh = {
     enable = true;
-    enableAutosuggestions = true;
     enableCompletion = true;
+    # enableAutoSuggestions = true;
+    dotDir = ".config/zsh";
+    # autosuggestions = {
+    #     enable = true;
+    #     highlight = true;
+    # };
     oh-my-zsh = {
       enable = true;
       plugins = [ "git" "terraform" "vi-mode"];
       theme = "robbyrussell";
     };
+    shellAliases = {
+        ls = "eza --long --header --git";
+        la = "eza --long --header --git -a";
+    };
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
   };
 
   home.packages = with pkgs; [
-    fzf
+    eza
   ];
 }
