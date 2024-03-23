55a2951322c8a4da587e9af63481f2988685ece4
zshen <zhongjie.x.shen@gmail.com>
2024-03-23 09:15:29 -0600

    update tmux config

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.tmux = {
@@ -8,10 +8,23 @@
     keyMode = "vi";
     newSession = true;
     historyLimit = 100000;
+    clock24 = true;
     extraConfig = ''
-        set -g detach-on-destroy off     # don't exit from tmux when closing a session \n
-        set -g renumber-windows on       # renumber all windows when any window is closed \n
-        set -g status-left-length 10 # could be any number
+    set -g detach-on-destroy off     # don't exit from tmux when closing a session \n
+    set -g renumber-windows on       # renumber all windows when any window is closed \n
+    set -g status-left-length 10     # could be any number
     '';
+    plugins = with pkgs; [
+      {
+        plugin = tmuxPlugins.dracula;
+        extraConfig = ''
+        set -g @plugin 'dracula/tmux'
+        set -g @dracula-show-powerline false
+        set -g @dracula-show-flags false
+        set -g @dracula-show-left-icon session
+        set -g @dracula-plugins "battery"
+        '';
+      }
+    ];
   };
 }
