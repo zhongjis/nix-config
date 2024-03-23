5f5ac9742fa06442240129706fe71570ee008b56
zshen <zhongjie.x.shen@gmail.com>
2024-03-23 09:29:21 -0600

    swtich back to unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:nixos/nixos-hardware/master";
 
     home-manager = {
