a4cef3eebf71949c1d57ef99c21a78f0886e65f9
zshen <zhongjie.x.shen@gmail.com>
2024-03-23 08:55:11 -0600

    update

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -7,5 +7,11 @@
     escapeTime = 0;
     keyMode = "vi";
     newSession = true;
+    historyLimit = 100000;
+    extraConfig = ''
+        set -g detach-on-destroy off     # don't exit from tmux when closing a session \n
+        set -g renumber-windows on       # renumber all windows when any window is closed \n
+        set -g status-left-length 10 # could be any number
+    '';
   };
 }
