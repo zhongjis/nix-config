eba55123a637bb856522be44b63f701376816157
zshen <zhongjie.x.shen@gmail.com>
2024-03-27 23:16:10 -0600

    restructure window-manager

--- modules/home-manager/window-manager/default.nix
+++ modules/home-manager/window-manager/default.nix
@@ -1,9 +1,9 @@
-let
-    hyprland = import ./hyprland.nix;
-    waybar = import ./waybar.nix;
-    rofi = import ./rofi.nix;
-in
-{
-    windowManager.rofi = rofi;
-}
+{ ... }:
 
+{
+    imports = [
+        ./hyprland.nix
+        ./waybar.nix
+        ./rofi.nix
+    ];
+}
