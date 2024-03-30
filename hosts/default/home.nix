15270ef1a527829e03ca1eca1b96a63a2c6325f0
zshen <zhongjie.x.shen@gmail.com>
2024-03-29 21:14:04 -0600

    add dolphin

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -38,6 +38,7 @@
     obsidian
     firefox
     font-manager
+    kdePackages.dolphin
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
