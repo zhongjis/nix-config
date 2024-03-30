e56946250aac5e245ef95d966ec3f1c06cefc0f1
zshen <zhongjie.x.shen@gmail.com>
2024-03-29 20:44:55 -0600

    update

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -129,9 +129,6 @@
   ];
 
   # hyprland related settings
-  programs.hyprland.enable = true;
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   # environment.sessionVariables = {
   #   NIXOS_OZONE_WL = "1";
   # };
