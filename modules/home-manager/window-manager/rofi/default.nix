75c3ea5e257f03945ef9a38a87af1b5d070ac256
zshen <zhongjie.x.shen@gmail.com>
2024-03-30 12:51:55 -0600

    update

--- modules/home-manager/window-manager/rofi/default.nix
+++ modules/home-manager/window-manager/rofi/default.nix
@@ -9,7 +9,17 @@
 
   home.packages = with pkgs; [
     (pkgs.writeScriptBin "rofi-toggle" ''
-      ${./rofi-toggle.sh}/bin/bash ${./rofi-toggle.sh} 
-    '')
+    #!/usr/bin/env bash
+
+    if pgrep -x "rofi" > /dev/null; then
+        # Rofi is running, kill it
+        pkill rofi 
+    else
+        # Rofi not running, launch it
+        rofi -show drun 
+        sleep 0.2           # Small delay to let Rofi open
+        hyprctl dispatch focuswindow active
+    fi
+  '')
   ];
 }
