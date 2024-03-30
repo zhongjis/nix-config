53f8d233c25ab7b9030880f5c3a5134dd6082175
zshen <zhongjie.x.shen@gmail.com>
2024-03-30 12:46:16 -0600

    externalize rofi-toggle script

--- modules/home-manager/window-manager/rofi/default.nix
+++ modules/home-manager/window-manager/rofi/default.nix
@@ -3,21 +3,13 @@
 {
   programs.rofi = {
     enable = true;
+    package = pkgs.rofi-wayland;
     terminal = "${pkgs.alacritty}/bin/alacritty";
   };
 
   home.packages = with pkgs; [
     (pkgs.writeScriptBin "rofi-toggle" ''
-    #!/usr/bin/env bash
-
-    if pgrep -x "rofi" > /dev/null; then
-        # Rofi is running, kill it
-        pkill rofi 
-    else
-        # Rofi not running, launch it
-        rofi -show drun 
-    fi
-  '')
+      ${./rofi-toggle.sh}/bin/bash ${./rofi-toggle.sh} 
+    '')
   ];
-
 }
