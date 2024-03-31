acca7c2fa96c38122e79eae42ff0b388285e3313
zshen <zhongjie.x.shen@gmail.com>
2024-03-30 22:16:21 -0600

    update nvim config

--- modules/home-manager/nvim/default.nix
+++ modules/home-manager/nvim/default.nix
@@ -1,31 +1,42 @@
 { pkgs, ... }:
 
 {
-  programs.neovim = {
+  programs.neovim = 
+  let
+    toLua = str: "lua << EOF\n${str}\nEOF\n";
+    toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
+  in
+  {
     enable = true;
     viAlias = true;
     vimAlias = true;
     vimdiffAlias = true;
 
     extraPackages = with pkgs; [
-        lazygit
         ripgrep
         nodejs_21
         unzip
         cargo
     ];
 
-    extraLuaConfig = ''
-        vim.o.scrolloff = 8
-        vim.o.number = true
-        vim.o.cursorline = true
-        vim.o.tabstop = 4
-        vim.o.softtabstop = 4
-        vim.o.shiftwidth = 4
-        vim.o.expandtab = true
-        vim.o.smartindent = true
+    plugins = with pkgs.vimPlugins; [
+      # {
+      #   plugin = comment-nvim;
+      #   config = toLua "require(\"Comment\").setup()";
+      # }
 
-        ${builtins.readFile ./kickstart.lua}
+      # {
+      #   plugin = nvim-lspconfig;
+      #   config = toLuaFile ./nvim/plugin/lsp.lua;
+      # }
+    ];
+
+    extraLuaConfig = ''
+      ${builtins.readFile ./kickstart.lua}
     '';
   };
+
+  home.packages = with pkgs; [
+      lazygit
+  ];
 }
