6aeea5598050fc078b53ebef67fbe449ff2bf9c9
zshen <zhongjie.x.shen@gmail.com>
2024-04-02 22:47:36 -0600

    externalize xremap configs

---
+++ modules/nixos/xremap.nix
@@ -0,0 +1,17 @@
+{ inputs }:
+
+{
+  imports = [
+    inputs.xremap-flake.homeManagerModules.default
+  ];
+
+  services.xremap = {
+    withWlroots = true;
+    yamlConfig = ''
+      modmap:
+        - name: main remaps
+          remap:
+            CapsLock: esc
+    '';
+  };
+}
