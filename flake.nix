853ac42ee4de8b6189667f54df374b953fe405b6
Zhongjie Shen <zshen@adobe.com>
2024-04-06 15:50:54 -0400

    minimal setup working

--- flake.nix
+++ flake.nix
@@ -19,10 +19,39 @@
 
   outputs = { self, nixpkgs, nix-darwin, nixos-hardware, ... }@inputs:
     let
-        systems = [ "x86_64-linux" "aarch64-darwin" ];
-        forEachSupportedSystem = f: nixpkgs.lib.genAttrs systems (system: f {
+      systems = [ "x86_64-linux" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs systems (system: f {
         pkgs = import nixpkgs { inherit system; };
       });
+
+      mac-configuration = { pkgs, ... }: {
+        # List packages installed in system profile. To search by name, run:
+        # $ nix-env -qaP | grep wget
+        environment.systemPackages =
+          [ pkgs.vim
+          ];
+
+        # Auto upgrade nix package and the daemon service.
+        services.nix-daemon.enable = true;
+        # nix.package = pkgs.nix;
+
+        # Necessary for using flakes on this system.
+        nix.settings.experimental-features = "nix-command flakes";
+
+        # Create /etc/zshrc that loads the nix-darwin environment.
+        programs.zsh.enable = true;  # default shell on catalina
+        # programs.fish.enable = true;
+
+        # Set Git commit hash for darwin-version.
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        system.stateVersion = 4;
+
+        # The platform the configuration will be used on.
+        nixpkgs.hostPlatform = "aarch64-darwin";
+      };
     in
     {
       nixosConfigurations = {
@@ -39,7 +68,8 @@
       darwinConfigurations = {
         mac-work = nix-darwin.lib.darwinSystem { 
           modules = [
-            ./hosts/work-mac/configuration.nix
+            # ./hosts/work-mac/configuration.nix 
+            mac-configuration
           ]; 
         };
       };
