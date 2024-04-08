25c16f94b1f38e994d08f7a023af82de9dbb5151
Zhongjie Shen <zshen@adobe.com>
2024-04-08 13:36:24 -0400

    various fixes
    1. move term color to tmux
    2. add toggle-alacritty

--- modules/home-manager/tmux/default.nix
+++ modules/home-manager/tmux/default.nix
@@ -6,23 +6,27 @@
     baseIndex = 1;
     escapeTime = 0;
     keyMode = "vi";
-    newSession = true;
+    newSession = false;
     historyLimit = 100000;
     clock24 = true;
     extraConfig = ''
-    set -g detach-on-destroy off     # don't exit from tmux when closing a session \n
-    set -g renumber-windows on       # renumber all windows when any window is closed \n
-    set -g status-left-length 15     # could be any number
+      # true color https://gist.github.com/andersevenrud/015e61af2fd264371032763d4ed965b6
+      set -g default-terminal "tmux-256color"
+      set -ag terminal-overrides ",xterm-256color:RGB"
+
+      set -g detach-on-destroy off     # don't exit from tmux when closing a session \n
+      set -g renumber-windows on       # renumber all windows when any window is closed \n
+      set -g status-left-length 15     # could be any number
     '';
     plugins = with pkgs; [
       {
         plugin = tmuxPlugins.dracula;
         extraConfig = ''
-        set -g @plugin 'dracula/tmux'
-        set -g @dracula-show-powerline false
-        set -g @dracula-show-flags false
-        set -g @dracula-show-left-icon session
-        set -g @dracula-plugins "battery"
+          set -g @plugin 'dracula/tmux'
+          set -g @dracula-show-powerline false
+          set -g @dracula-show-flags false
+          set -g @dracula-show-left-icon session
+          set -g @dracula-plugins "battery"
         '';
       }
     ];
@@ -30,7 +34,7 @@
 
   home.packages = with pkgs; [
     (pkgs.writeScriptBin "tmux-sessionizer" ''
-    ${builtins.readFile ./tmux-sessionizer.sh}
-  '')
+      ${builtins.readFile ./tmux-sessionizer.sh}
+    '')
   ];
 }
