d7e4c282ca675ea35bdffc53497b953e11d81ef7
Zhongjie Shen <zshen@adobe.com>
2024-04-10 11:20:17 -0600

    add 1.5.2 terraform

--- hosts/work-mac/configuration.nix
+++ hosts/work-mac/configuration.nix
@@ -1,4 +1,15 @@
 { inputs, pkgs, ... }:
+let
+     pkgs = import (builtins.fetchGit {
+         # Descriptive name to make the store path easier to identify
+         name = "terraform-1-5-2";
+         url = "https://github.com/NixOS/nixpkgs/";
+         ref = "refs/heads/nixpkgs-unstable";
+         rev = "5a8650469a9f8a1958ff9373bd27fb8e54c4365d";
+     }) {};
+
+     myPkg = pkgs.terraform;
+in
 {
   imports = 
     [ 
@@ -72,8 +83,8 @@
 
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
-  environment.systemPackages = with pkgs; [
-    terraform
+  environment.systemPackages = [
+    myPkg.terraform
   ];
 
   # Auto upgrade nix package and the daemon service.
