5ce1089b1d1e1847bca5bd3680a09996ceaa0b42
Zhongjie Shen <zshen@adobe.com>
2024-04-16 22:33:38 -0600

    update

--- modules/home-manager/common/neovim/plugins/lsp.lua
+++ modules/home-manager/common/neovim/plugins/lsp.lua
@@ -40,27 +40,15 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
 local servers = {
-  -- clangd = {},
-  -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
-  -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-  --
-  -- Some languages (like typescript) have entire language plugins that can be useful:
-  --    https://github.com/pmizio/typescript-tools.nvim
-  --
-  -- But for many setups, the LSP (`tsserver`) will work just fine
-  -- tsserver = {},
-  nil_ls = {},
+  nil_ls = {
+    capabilities = capabilities,
+  },
   jsonls = {},
   yamlls = {},
   terraformls = {},
   tflint = {},
   dockerls = {},
   lua_ls = {
-    -- cmd = {...},
-    -- filetypes = { ...},
-    -- capabilities = {},
     settings = {
       Lua = {
         completion = {
