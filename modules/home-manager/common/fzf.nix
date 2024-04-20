fbbecd19e0f822eef45d3a535bc3826f8631d763
Zhongjie Shen <zshen@adobe.com>
2024-04-19 20:32:38 -0600

    add fzf.nix

---
+++ modules/home-manager/common/fzf.nix
@@ -0,0 +1,28 @@
+{ lib, config, ... }:
+{
+  optoins = {
+    fzf.enalbe =
+      lib.mkEnableOption "enable fzf";
+  };
+
+  config = lib.mkIf config.fzf.enable {
+    programs.fzf = {
+      enable = true;
+      enableZshIntegration = true;
+      colors = {
+        "bg+" = "#313244";
+        bg = "#1e1e2e";
+        spinner = "#f5e0dc";
+        hl = "#f38ba8";
+        fg = "#cdd6f4";
+        header = "#f38ba8";
+        info = "#cba6f7";
+        pointer = "#f5e0dc";
+        marker = "#f5e0dc";
+        "fg+" = "#cdd6f4";
+        prompt = "#cba6f7";
+        "hl+" = "#f38ba8";
+      };
+    };
+  };
+}
