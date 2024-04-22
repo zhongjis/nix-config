17be87c56594e19320bf92028cea0df6d1421e8b
Zhongjie Shen <zshen@adobe.com>
2024-04-22 00:09:56 -0600

    update

--- modules/home-manager/common/lazygit/default.nix
+++ modules/home-manager/common/lazygit/default.nix
@@ -7,27 +7,11 @@
   };
 
   config = lib.mkIf config.lazygit.enable {
+    xdg.enable = true;
     programs.lazygit = {
       enable = true;
-      settings = {
-        gui = {
-          # NOTE: https://github.com/catppuccin/lazygit/tree/main
-          theme = {
-            activeBorderColor = [ "#89b4fa" "bold" ];
-            inactiveBorderColor = [ "#a6adc8" ];
-            optionsTextColor = [ "#89b4fa" ];
-            selectedLineBgColor = [ "#313244" ];
-            cherryPickedCommitBgColor = [ "#45475a" ];
-            cherryPickedCommitFgColor = [ "#89b4fa" ];
-            unstagedChangesColor = [ "#f38ba8" ];
-            defaultFgColor = [ "#cdd6f4" ];
-            searchingActiveBorderColor = [ "#f9e2af" ];
-          };
-          authorColor = {
-            "*" = "'#b4befe";
-          };
-        };
-      };
+      catppuccin.enable = true;
+      catppuccin.flavour = "mocha";
     };
   };
 }
