e47c04be798a9c842a501abb12f82c51c4a9764c
Zhongjie Shen <zshen@adobe.com>
2024-04-29 23:58:26 -0600

    add sketchybar for mac

---
+++ modules/nix-darwin/skechybar.nix
@@ -0,0 +1,16 @@
+{ lib, config, ... }:
+
+{
+  options = {
+    sketchybar.enable =
+      lib.mkEnableOption "enables sketchybar";
+  };
+
+  config = lib.mkIf config.sketchybar.enable {
+    system.defaults.NSGlobalDomain._HIHideMenuBar = true;
+
+    services.sketchybar = {
+      enable = true;
+    };
+  };
+}
