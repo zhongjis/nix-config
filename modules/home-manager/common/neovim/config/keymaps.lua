0cc7c782649fe36d20e7eb28c2c34099917b84d5
Zhongjie Shen <zshen@adobe.com>
2024-04-30 23:23:36 -0600

    refactor keymap.lua

--- modules/home-manager/common/neovim/config/keymaps.lua
+++ modules/home-manager/common/neovim/config/keymaps.lua
@@ -1,175 +1,105 @@
 local map = function(keys, func, desc)
-  vim.keymap.set("n", keys, func, { buffer = bufnr, desc = "LSP: " .. desc })
+  vim.keymap.set("n", keys, func, { desc = desc })
 end
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+map("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- **diagnostic**
-vim.keymap.set(
-  "n",
-  "[d",
-  vim.diagnostic.goto_prev,
-  { desc = "Go to previous [D]iagnostic message" }
-)
-vim.keymap.set(
-  "n",
-  "]d",
-  vim.diagnostic.goto_next,
-  { desc = "Go to next [D]iagnostic message" }
-)
-vim.keymap.set(
-  "n",
-  "<leader>e",
-  vim.diagnostic.open_float,
-  { desc = "Show diagnostic [E]rror messages" }
-)
+map("[d", vim.diagnostic.goto_prev, "Go to previous [D]iagnostic message")
+map("]d", vim.diagnostic.goto_next, "Go to next [D]iagnostic message")
+map("<leader>e", vim.diagnostic.open_float, "Show diagnostic [E]rror messages")
 
 -- **oil**
-vim.keymap.set(
-  "n",
-  "<leader>o",
-  "<CMD>Oil<CR>",
-  { desc = "[O]il: Open parent directory" }
-)
+map("<leader>o", "<CMD>Oil<CR>", "[O]il: Open parent directory")
 
 -- **telescope**
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-vim.keymap.set(
-  "n",
-  "<leader>ss",
-  builtin.builtin,
-  { desc = "[S]earch [S]elect Telescope" }
-)
-vim.keymap.set(
-  "n",
-  "<leader>sw",
-  builtin.grep_string,
-  { desc = "[S]earch current [W]ord" }
-)
-vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-vim.keymap.set(
-  "n",
-  "<leader>sd",
-  builtin.diagnostics,
-  { desc = "[S]earch [D]iagnostics" }
-)
-vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
-vim.keymap.set(
-  "n",
-  "<leader>s.",
-  builtin.oldfiles,
-  { desc = '[S]earch Recent Files ("." for repeat)' }
-)
-vim.keymap.set(
-  "n",
-  "<leader><leader>",
-  builtin.buffers,
-  { desc = "[ ] Find existing buffers" }
-)
-vim.keymap.set(
-  "n",
+map("<leader>sh", builtin.help_tags, "[S]earch [H]elp")
+map("<leader>sk", builtin.keymaps, "[S]earch [K]eymaps")
+map("<leader>sf", builtin.find_files, "[S]earch [F]iles")
+map("<leader>ss", builtin.builtin, "[S]earch [S]elect Telescope")
+map("<leader>sw", builtin.grep_string, "[S]earch current [W]ord")
+map("<leader>sg", builtin.live_grep, "[S]earch by [G]rep")
+map("<leader>sd", builtin.diagnostics, "[S]earch [D]iagnostics")
+map("<leader>sr", builtin.resume, "[S]earch [R]esume")
+map("<leader>s.", builtin.oldfiles, '[S]earch Recent Files ("." for repeat)')
+map("<leader><leader>", builtin.buffers, "[ ] Find existing buffers")
+map(
   "<leader>/",
   builtin.current_buffer_fuzzy_find,
-  { desc = "[/] Fuzzily search in current buffer" }
+  "[/] Fuzzily search in current buffer"
 )
-vim.keymap.set("n", "<leader>s/", function()
+map("<leader>s/", function()
   builtin.live_grep({
     grep_open_files = true,
     prompt_title = "Live Grep in Open Files",
   })
-end, { desc = "[S]earch [/] in Open Files" })
+end, "[S]earch [/] in Open Files")
 
 -- TODO: migrate this to use nix-config's
--- vim.keymap.set("n", "<leader>sn", function()
---   builtin.find_files({ cwd = vim.fn.stdpath("config") })
--- end, { desc = "[S]earch [N]eovim files" })
+-- map("n", "<leader>sn", function()
+--   builtin.find_files({ cwd = vim.fn.stdpath("config") )
+-- end, "[S]earch [N]eovim files" )
 
 -- **harpoon**
 local harpoon = require("harpoon")
-vim.keymap.set("n", "<leader>a", function()
+map("<leader>a", function()
   harpoon:list():add()
-end, { desc = "Harpoon: [a]dd file to list" })
-vim.keymap.set("n", "<leader>h", function()
+end, "Harpoon: [a]dd file to list")
+map("<leader>h", function()
   harpoon.ui:toggle_quick_menu(harpoon:list())
-end, { desc = "[H]arpoon: quick menu" })
-vim.keymap.set("n", "<c-h>", function()
+end, "[H]arpoon: quick menu")
+map("<c-h>", function()
   harpoon:list():select(1)
-end, { desc = "Harpoon: go to file 1" })
-vim.keymap.set("n", "<c-j>", function()
+end, "Harpoon: go to file 1")
+map("<c-j>", function()
   harpoon:list():select(2)
-end, { desc = "Harpoon: go to file 2" })
-vim.keymap.set("n", "<c-k>", function()
+end, "Harpoon: go to file 2")
+map("<c-k>", function()
   harpoon:list():select(3)
-end, { desc = "Harpoon: go to file 3" })
-vim.keymap.set("n", "<c-l>", function()
+end, "Harpoon: go to file 3")
+map("<c-l>", function()
   harpoon:list():select(4)
-end, { desc = "Harpoon: go to file 4" })
+end, "Harpoon: go to file 4")
 
 -- **conform**
-vim.keymap.set("n", "<leader>f", function()
+map("<leader>f", function()
   require("conform").format({ async = true, lsp_fallback = true })
-end, { desc = "[F]ormat buffer" })
+end, "[F]ormat buffer")
 
 -- **lazygit.nvim**
-vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<CR>", { desc = "LazyGit" })
+map("<leader>gg", "<cmd>LazyGit<CR>", "LazyGit")
 
 -- **undotree**
-vim.keymap.set(
-  "n",
-  "<leader>u",
-  "<cmd>UndotreeToggle<CR>",
-  { desc = "[U]ndotree Toggle" }
-)
+map("<leader>u", "<cmd>UndotreeToggle<CR>", "[U]ndotree Toggle")
 
 -- **trouble.nvim**
-vim.keymap.set(
-  "n",
-  "<leader>td",
-  "<cmd>Trouble diagnostics toggle<cr>",
-  { desc = "[T]rouble: [D]iagnostics" }
-)
-vim.keymap.set(
-  "n",
+map("<leader>td", "<cmd>Trouble diagnostics toggle<cr>", "[T]rouble: [D]iagnostics")
+map(
   "<leader>tb",
   "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-  { desc = "[T]rouble: [B]uffer Diagnostics" }
+  "[T]rouble: [B]uffer Diagnostics"
 )
-vim.keymap.set(
-  "n",
+map(
   "<leader>tt",
   "<cmd>Trouble symbols toggle focus=false<cr>",
-  { desc = "[T]rouble: [T]oggle Symbol)" }
+  "[T]rouble: [T]oggle Symbol)"
 )
-vim.keymap.set(
-  "n",
+map(
   "<leader>tl",
   "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-  { desc = "[T]rouble: [L]SP Definitions / references / ..." }
+  "[T]rouble: [L]SP Definitions / references / ..."
 )
-vim.keymap.set(
-  "n",
-  "<leader>tl",
-  "<cmd>Trouble loclist toggle<cr>",
-  { desc = "[T]rouble: [L]ocation List" }
-)
-vim.keymap.set(
-  "n",
-  "<leader>tq",
-  "<cmd>Trouble qflist toggle<cr>",
-  { desc = "[T]rouble: [Q]uickfix List" }
-)
-vim.keymap.set("n", "[t", function()
+map("<leader>tl", "<cmd>Trouble loclist toggle<cr>", "[T]rouble: [L]ocation List")
+map("<leader>tq", "<cmd>Trouble qflist toggle<cr>", "[T]rouble: [Q]uickfix List")
+map("[t", function()
   require("trouble").next({ skip_groups = true, jump = true })
-end, { desc = "Go to previous [D]iagnostic message" })
-vim.keymap.set("n", "]t", function()
+end, "Go to previous [D]iagnostic message")
+map("]t", function()
   require("trouble").next({ skip_groups = true, jump = true })
-end, { desc = "Go to next [D]iagnostic message" })
+end, "Go to next [D]iagnostic message")
 
 -- **zen-mode.nvim**
-vim.keymap.set("n", "<leader>zz", function()
-  require("zen-mode").toggle({
-    width = 0.85, -- width will be 85% of the editor width
-  })
-end, { desc = "Toggle [Z]en Mode" })
+map("<leader>zz", function()
+  require("zen-mode").toggle({ width = 0.85 })
+end, "Toggle [Z]en Mode")
