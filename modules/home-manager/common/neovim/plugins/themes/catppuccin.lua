99dc123b772fe08dc07870265b12559cc8ffa044
Zhongjie Shen <zshen@adobe.com>
2024-05-04 10:38:59 -0600

    move neovim themes setup to separate folder

---
+++ modules/home-manager/common/neovim/plugins/themes/catppuccin.lua
@@ -0,0 +1,21 @@
+-- NOTE: https://github.com/catppuccin/nvim
+require("catppuccin").setup({
+  flavour = "mocha",
+  integrations = {
+    cmp = true,
+    gitsigns = true,
+    -- nvimtree = true,
+    treesitter = true,
+    mini = {
+      enabled = true,
+      indentscope_color = "",
+    },
+    which_key = true,
+    fidget = true,
+    harpoon = true,
+    mason = true,
+    -- noice = true,
+    -- notify = true,
+    -- lsp_trouble = true,
+  },
+})
---
+++ modules/home-manager/common/neovim/plugins/themes/solarized-osaka.lua
@@ -0,0 +1,43 @@
+require("solarized-osaka").setup({
+  styles = {
+    -- Style to be applied to different syntax groups
+    -- Value is any valid attr-list value for `:help nvim_set_hl`
+    comments = { italic = true },
+    keywords = { italic = true },
+    functions = {},
+    variables = {},
+    -- Background styles. Can be "dark", "transparent" or "normal"
+    sidebars = "transparent", -- style for sidebars, see below
+    floats = "transparent", -- style for floating windows
+  },
+  on_highlights = function(hl, c)
+    local prompt = "#2d3149"
+    hl.TelescopeNormal = {
+      bg = c.bg_dark,
+      fg = c.fg_dark,
+    }
+    hl.TelescopeBorder = {
+      bg = c.bg_dark,
+      fg = c.bg_dark,
+    }
+    hl.TelescopePromptNormal = {
+      bg = prompt,
+    }
+    hl.TelescopePromptBorder = {
+      bg = prompt,
+      fg = prompt,
+    }
+    hl.TelescopePromptTitle = {
+      bg = prompt,
+      fg = prompt,
+    }
+    hl.TelescopePreviewTitle = {
+      bg = c.bg_dark,
+      fg = c.bg_dark,
+    }
+    hl.TelescopeResultsTitle = {
+      bg = c.bg_dark,
+      fg = c.bg_dark,
+    }
+  end,
+})
