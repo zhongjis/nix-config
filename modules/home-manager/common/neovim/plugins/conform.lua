250ebf0b5e676282ff45d728497cbc24b525c145
Zhongjie Shen <zshen@adobe.com>
2024-05-23 14:46:39 -0600

    update

--- modules/home-manager/common/neovim/plugins/conform.lua
+++ modules/home-manager/common/neovim/plugins/conform.lua
@@ -3,6 +3,11 @@ local conform = require("conform")
 conform.setup({
   notify_on_error = false,
   format_on_save = function(bufnr)
+    -- Disable with a global or buffer-local variable
+    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+      return
+    end
+
     -- Disable "format_on_save lsp_fallback" for languages that don't
     -- have a well standardized coding style. You can add additional
     -- languages here or re-enable it for the disabled ones.
@@ -54,3 +59,21 @@ conform.formatters.shfmt = {
 --     return {}
 --   end,
 -- }
+
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+  if args.bang then
+    -- FormatDisable! will disable formatting just for this buffer
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = "Disable autoformat-on-save",
+  bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = "Re-enable autoformat-on-save",
+})
--- modules/nix-darwin/default.nix
+++ modules/nix-darwin/default.nix
@@ -7,5 +7,5 @@
 
   skhd.enable = lib.mkDefault true;
   yabai.enable = lib.mkDefault true;
-  sketchybar.enable = lib.mkDefault true;
+  sketchybar.enable = lib.mkDefault false;
 }
