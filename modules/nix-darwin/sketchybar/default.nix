d2d890b46f7f23e90afde1ffdaa2b55f776baafa
Zhongjie Shen <zshen@adobe.com>
2024-05-22 23:32:45 -0600

    update sketchybar

--- modules/nix-darwin/sketchybar/default.nix
+++ modules/nix-darwin/sketchybar/default.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: {
   options = {
@@ -17,7 +18,37 @@
         ${builtins.readFile ./sketchybarrc}
       '';
 
-      extraPackages = with pkgs; {};
+      home.file.".config/sketchybar/plugins/battery.sh".text = ''
+        ${builtins.readFile ./plugins/battery.sh}
+      '';
+
+      home.file.".config/sketchybar/plugins/spotify.sh".text = ''
+        ${builtins.readFile ./plugins/spotify.sh}
+      '';
+
+      home.file.".config/sketchybar/plugins/clock.sh".text = ''
+        ${builtins.readFile ./plugins/clock.sh}
+      '';
+
+      home.file.".config/sketchybar/plugins/current_space.sh".text = ''
+        ${builtins.readFile ./plugins/current_space.sh}
+      '';
+
+      home.file.".config/sketchybar/plugins/front_app.sh".text = ''
+        ${builtins.readFile ./plugins/front_app.sh}
+      '';
+
+      home.file.".config/sketchybar/plugins/volume.sh".text = ''
+        ${builtins.readFile ./plugins/volume.sh}
+      '';
+
+      home.file.".config/sketchybar/plugins/weather.sh".text = ''
+        ${builtins.readFile ./plugins/weather.sh}
+      '';
+
+      extraPackages = with pkgs; [
+        jq
+      ];
     };
   };
 }
