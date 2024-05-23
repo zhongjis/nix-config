e91e8db1a02b94b1ff0b7b3b96030be26b9a57e2
Zhongjie Shen <zshen@adobe.com>
2024-05-23 00:20:09 -0600

    update

--- modules/nix-darwin/sketchybar/default.nix
+++ modules/nix-darwin/sketchybar/default.nix
@@ -3,7 +3,58 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  inherit (pkgs) writeTextFile;
+
+  batteryScript = writeTextFile {
+    name = "sketchybar-battery.sh";
+    text = ''
+      ${builtins.readFile ./plugins/battery.sh}
+    '';
+  };
+
+  spotifyScript = writeTextFile {
+    name = "sketchybar-spotify.sh";
+    text = ''
+      ${builtins.readFile ./plugins/spotify.sh}
+    '';
+  };
+
+  clockScript = writeTextFile {
+    name = "sketchybar-clock.sh";
+    text = ''
+      ${builtins.readFile ./plugins/clock.sh}
+    '';
+  };
+
+  current_spaceScript = writeTextFile {
+    name = "sketchybar-current-space.sh";
+    text = ''
+      ${builtins.readFile ./plugins/current_space.sh}
+    '';
+  };
+
+  front_appScript = writeTextFile {
+    name = "sketchybar-front-app.sh";
+    text = ''
+      ${builtins.readFile ./plugins/front_app.sh}
+    '';
+  };
+
+  volumeScript = writeTextFile {
+    name = "sketchybar-volume.sh";
+    text = ''
+      ${builtins.readFile ./plugins/volume.sh}
+    '';
+  };
+
+  weatherScript = writeTextFile {
+    name = "sketchybar-weather.sh";
+    text = ''
+      ${builtins.readFile ./plugins/weather.sh}
+    '';
+  };
+in {
   options = {
     sketchybar.enable =
       lib.mkEnableOption "enables sketchybar";
@@ -18,37 +69,19 @@
         ${builtins.readFile ./sketchybarrc}
       '';
 
-      home.file.".config/sketchybar/plugins/battery.sh".text = ''
-        ${builtins.readFile ./plugins/battery.sh}
-      '';
-
-      home.file.".config/sketchybar/plugins/spotify.sh".text = ''
-        ${builtins.readFile ./plugins/spotify.sh}
-      '';
-
-      home.file.".config/sketchybar/plugins/clock.sh".text = ''
-        ${builtins.readFile ./plugins/clock.sh}
-      '';
-
-      home.file.".config/sketchybar/plugins/current_space.sh".text = ''
-        ${builtins.readFile ./plugins/current_space.sh}
-      '';
-
-      home.file.".config/sketchybar/plugins/front_app.sh".text = ''
-        ${builtins.readFile ./plugins/front_app.sh}
-      '';
-
-      home.file.".config/sketchybar/plugins/volume.sh".text = ''
-        ${builtins.readFile ./plugins/volume.sh}
-      '';
-
-      home.file.".config/sketchybar/plugins/weather.sh".text = ''
-        ${builtins.readFile ./plugins/weather.sh}
-      '';
-
       extraPackages = with pkgs; [
         jq
       ];
+    };
+
+    environment.etc = {
+      ".config/sketchybar/plugins/battery.sh".source = "${batteryScript}";
+      ".config/sketchybar/plugins/spotify.sh".source = "${spotifyScript}";
+      ".config/sketchybar/plugins/clock.sh".source = "${clockScript}";
+      ".config/sketchybar/plugins/current_space.sh".source = "${current_spaceScript}";
+      ".config/sketchybar/plugins/front_app.sh".source = "${front_appScript}";
+      ".config/sketchybar/plugins/volume.sh".source = "${volumeScript}";
+      ".config/sketchybar/plugins/weather.sh".source = "${weatherScript}";
     };
   };
 }
