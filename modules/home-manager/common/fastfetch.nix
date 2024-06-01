fe0002606c688ef6c9b036bea6b34e6825dc9cc6
Zhongjie Shen <zshen@adobe.com>
2024-06-01 08:49:37 -0600

    add fastfetch

---
+++ modules/home-manager/common/fastfetch.nix
@@ -0,0 +1,44 @@
+{
+  lib,
+  config,
+  ...
+}: {
+  options = {
+    fastfetch.enable =
+      lib.mkEnableOption "enable fastfetch";
+  };
+
+  config = lib.mkIf config.fastfetch.enable {
+    programs.fastfetch = {
+      enable = true;
+      settings = {
+        logo = {
+          source = "nixos_small";
+          padding = {
+            right = 1;
+          };
+        };
+        display = {
+          binaryPrefix = "si";
+          color = "blue";
+          separator = "  ";
+        };
+        modules = [
+          {
+            type = "datetime";
+            key = "Date";
+            format = "{1}-{3}-{11}";
+          }
+          {
+            type = "datetime";
+            key = "Time";
+            format = "{14}:{17}:{20}";
+          }
+          "break"
+          "player"
+          "media"
+        ];
+      };
+    };
+  };
+}
