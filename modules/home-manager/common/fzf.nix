d038c51d38946f1ae557a16d61472321b42659cf
Zhongjie Shen <zshen@adobe.com>
2024-08-01 15:04:58 -0600

    replace nix-catppuccin for fzf with manual

--- modules/home-manager/common/fzf.nix
+++ modules/home-manager/common/fzf.nix
@@ -12,14 +12,26 @@
   config = lib.mkIf config.fzf.enable {
     programs.fzf = {
       enable = true;
-      catppuccin.enable = true;
-      catppuccin.flavor = "mocha";
 
       enableZshIntegration = true;
       changeDirWidgetCommand = "fd --type -d";
       changeDirWidgetOptions = [
         "--preview 'tree -C {} | head -200'"
       ];
+      colors = {
+        bg = "#1e1e2e";
+        "bg+" = "#313244";
+        spinner = "#f5e0dc";
+        hl = "#f38ba8";
+        fg = "#cdd6f4";
+        header = "#f38ba8";
+        info = "#cba6f7";
+        pointer = "#f5e0dc";
+        marker = "#f5e0dc";
+        "fg+" = "#cdd6f4";
+        prompt = "#cba6f7";
+        "hl+" = "#f38ba8";
+      };
     };
 
     home.packages = with pkgs; [
