23c6b8b6a61979d7ae22f36dc4383291f9ce8cdd
zhongjis <zhongjie.x.shen@gmail.com>
2024-11-13 01:54:01 -0700

    init razer config

--- flake.nix
+++ flake.nix
@@ -78,5 +78,10 @@
       system = "x86_64-linux";
       darwin = false;
     };
+
+    homeConfigurations."zshen-razer" = mkHomeManager "razer-14" {
+      system = "x86_64-linux";
+      darwin = false;
+    };
   };
 }
---
+++ hosts/razer-14/home.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  pkgs,
+  lib,
+  inputs,
+  ...
+}: {
+  imports = [
+    ../../modules/home-manager
+  ];
+
+  # override common module options
+  git.enable = true;
+
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "zshen";
+  home.homeDirectory = "/home/zshen";
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+}
