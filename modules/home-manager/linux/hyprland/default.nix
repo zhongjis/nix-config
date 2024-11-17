6ee579114d09434debad11d946a4c5da23b53ca7
zhongjis <zhongjie.x.shen@gmail.com>
2024-11-17 09:41:55 +0000

    update hyprland config

--- modules/home-manager/linux/hyprland/default.nix
+++ modules/home-manager/linux/hyprland/default.nix
@@ -10,27 +10,6 @@
   };
 
   config = lib.mkIf config.hyprland.enable {
-    home.packages = with pkgs; [
-      dunst
-      lxqt.lxqt-policykit
-
-      brightnessctl # brightness control
-      pavucontrol # volume control GUI
-
-      wl-clipboard
-      cliphist
-    ];
-
-    xdg.portal.enable = true;
-    xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
-    xdg.portal.config.common.default = "*";
-
-    wayland.windowManager.hyprland = {
-      enable = true;
-      xwayland.enable = true;
-      extraConfig = ''
-        ${builtins.readFile ./hyprland-default.conf}
-      '';
-    };
+    xdg.configFile."hyprland/hyprland.conf".source = ./hyprland.conf;
   };
 }
