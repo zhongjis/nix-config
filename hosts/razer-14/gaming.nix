84c52b8aa3da0a8720e58e9e354d30d575b56b75
zhongjis <zhongjie.x.shen@gmail.com>
2024-11-19 07:23:39 +0000

    refactor

--- hosts/razer-14/gaming.nix
+++ hosts/razer-14/gaming.nix
@@ -26,11 +26,7 @@
         enableOffloadCmd = true;
       };
 
-      # integrated
-      # intelBusId = "PCI:0:0:0";
       amdgpuBusId = "PCI:4:0:0";
-
-      # dedicated
       nvidiaBusId = "PCI:1:0:0";
     };
   };
