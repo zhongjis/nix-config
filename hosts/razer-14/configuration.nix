4f98a4cd78ad12d73ba4d4e36d63b8461266b5d1
zhongjis <zhongjie.x.shen@gmail.com>
2024-11-21 18:00:32 +0000

    update

--- hosts/razer-14/configuration.nix
+++ hosts/razer-14/configuration.nix
@@ -14,6 +14,15 @@
     ./gaming.nix
   ];
 
+  # nh
+  programs.nh = {
+    enable = true;
+    package = pkgs.unstable.nh;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 30d --keep 10";
+    flake = "/home/zshen/personal/nix-config";
+  };
+
   # xremap
   hardware.uinput.enable = true;
   users.groups = {
