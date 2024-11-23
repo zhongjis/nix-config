9b67e9a97f85fbebb25a30a67e2a22c93711bf0a
zhongjis <zhongjie.x.shen@gmail.com>
2024-11-23 15:13:54 -0700

    update zsh config

--- modules/home-manager/features/zsh/default.nix
+++ modules/home-manager/features/zsh/default.nix
@@ -14,24 +14,17 @@
     then "nh home test . -c zshen-mac"
     else "nh home test . -c zshen-linux";
 in {
+  home.file = {
+    ".local/share/zsh/zsh-autosuggestions".source = "${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions";
+    ".local/share/zsh/zsh-fast-syntax-highlighting".source = "${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions";
+    ".local/share/zsh/nix-zsh-completions".source = "${pkgs.nix-zsh-completions}/share/zsh/plugins/nix";
+    ".local/share/zsh/zsh-vi-mode".source = "${pkgs.zsh-vi-mode}/share/zsh-vi-mode";
+  };
+
   programs.zsh = {
     enable = true;
     # enableCompletion = true;
     dotDir = ".config/zsh";
-    autosuggestion = {
-      enable = true;
-      highlight = "fg=#ff00ff,bg=cyan,bold,underline";
-    };
-    syntaxHighlighting.enable = true;
-    oh-my-zsh = {
-      enable = true;
-      plugins = [
-        "git"
-        "gh"
-        "sublime-merge"
-        "mvn"
-      ];
-    };
     history = {
       expireDuplicatesFirst = true;
       extended = true;
@@ -40,14 +33,37 @@ in {
     };
     shellAliases = {
       cat = "bat -p";
+      tree = "eza --color=auto --tree";
+      grep = "grep --color=auto";
+
       oswitch = oswitchCMD;
       otest = otestCMD;
       hswitch = hswitchCMD;
       htest = htestCMD;
     };
-    initExtra = ''
-      ${builtins.readFile ./catppuccin_mocha-zsh-syntax-highlighting.zsh}
-      bindkey -v
-    '';
+    initExtra =
+      /*
+      bash
+      */
+      ''
+        ${builtins.readFile ./catppuccin_mocha-zsh-syntax-highlighting.zsh}
+
+        # PLUGINS (whatever)
+        [ -f "$HOME/.local/share/zsh/zsh-vi-mode/zsh-vi-mode.plugin.zsh" ] && \
+        source "$HOME/.local/share/zsh/zsh-vi-mode/zsh-vi-mode.plugin.zsh"
+
+        [ -f "$HOME/.local/share/zsh/zsh-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh" ] && \
+        source "$HOME/.local/share/zsh/zsh-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
+
+        ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#ff00ff,bg=cyan,bold,underline"
+        bindkey '^ ' autosuggest-accept
+
+        [ -f "$HOME/.local/share/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" ] && \
+        source "$HOME/.local/share/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh"
+
+        [ -f "$HOME/.local/share/zsh/nix-zsh-completions/nix.plugin.zsh" ] && \
+        source "$HOME/.local/share/zsh/nix-zsh-completions/nix.plugin.zsh"
+
+      '';
   };
 }
