f0b7ae00f82c99622a02c0b05e17fa809e93dabb
zhongjis <zhongjie.x.shen@gmail.com>
2024-11-24 00:59:34 -0700

    update

--- modules/home-manager/features/tmux/default.nix
+++ modules/home-manager/features/tmux/default.nix
@@ -1,6 +1,7 @@
 {
   pkgs,
   isDarwin,
+  config,
   ...
 }: let
   keyboardCmd =
@@ -20,34 +21,41 @@ in {
     newSession = false;
     historyLimit = 100000;
     clock24 = true;
-    extraConfig = ''
-      set-option -g default-shell ${pkgs.zsh}/bin/zsh
+    shell = "${pkgs.zsh}/bin/zsh";
 
-      # pane movement like vim
-      bind-key l select-pane -L
-      bind-key k select-pane -D
-      bind-key i select-pane -U
-      bind-key j select-pane -R
+    extraConfig =
+      /*
+      bash
+      */
+      ''
+        unbind r
+        bind r source-file ${config.home.homeDirectory}/.config/tmux/tmux.conf
 
-      # true color
-      # https://gist.github.com/andersevenrud/015e61af2fd264371032763d4ed965b6
-      set -g default-terminal "tmux-256color"
-      set -ag terminal-overrides ",xterm-256color:RGB"
+        # Pane movement like vim
+        bind-key l select-pane -L
+        bind-key k select-pane -D
+        bind-key i select-pane -U
+        bind-key j select-pane -R
 
-      # Undercurl
-      set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'  # undercurl support
-      set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'  # underscore colours - needs tmux-3.0
+        # True color
+        # https://gist.github.com/andersevenrud/015e61af2fd264371032763d4ed965b6
+        set -g default-terminal "tmux-256color"
+        set -ag terminal-overrides ",xterm-256color:RGB"
 
-      # copy to clipboard
-      bind-key -T copy-mode-vi v send -X begin-selection
-      bind-key -T copy-mode-vi V send -X select-line
-      bind-key -T copy-mode-vi y send -X copy-pipe-and-cancel '${keyboardCmd}'
+        # Undercurl
+        set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'  # undercurl support
+        set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'  # underscore colours - needs tmux-3.0
 
-      # other settings
-      set -g detach-on-destroy off     # don't exit from tmux when closing a session \n
-      set -g renumber-windows on       # renumber all windows when any window is closed \n
-      set -g status-left-length 15     # could be any number
-    '';
+        # Copy to clipboard
+        bind-key -T copy-mode-vi v send -X begin-selection
+        bind-key -T copy-mode-vi V send -X select-line
+        bind-key -T copy-mode-vi y send -X copy-pipe-and-cancel '${keyboardCmd}'
+
+        # other settings
+        set -g detach-on-destroy off     # don't exit from tmux when closing a session \n
+        set -g renumber-windows on       # renumber all windows when any window is closed \n
+        set -g status-left-length 15     # could be any number
+      '';
   };
 
   home.packages = [
