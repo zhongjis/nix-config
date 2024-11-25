763a546a7b20b32a5576f0e32164f4847caf0493
zhongjis <zhongjie.x.shen@gmail.com>
2024-11-25 00:39:44 -0700

    refactor hyprland linux hm module

--- modules/home-manager-linux/features/hyprland/default.nix
+++ modules/home-manager-linux/features/hyprland/default.nix
@@ -5,6 +5,8 @@
 }: let
   uptime-sh = pkgs.writeShellScript "uptime-nixos" (builtins.readFile ./scripts/uptime-nixos.sh);
 in {
+  import = [./gtk.nix];
+
   myHomeManagerLinux.rofi.enable = true;
   myHomeManagerLinux.swaync.enable = true;
   myHomeManagerLinux.waybar.enable = true;
@@ -12,71 +14,13 @@ in {
 
   # hyprland
   xdg.configFile."hypr/hyprland.conf".source = ./hyprland.conf;
+  wayland.windowManager.hyprland.enable = true;
 
   home.pointerCursor = {
     # x11.enable = true;
     package = pkgs.bibata-cursors;
     name = "Bibata-Modern-Classic";
     size = 16;
-  };
-
-  home.packages = [
-    pkgs.nordic
-    pkgs.arc-icon-theme
-    pkgs.lxappearance
-  ];
-
-  home.file.".gtkrc-2.0" = {
-    text = ''
-      gtk-theme-name="Nordic-darker"
-      gtk-icon-theme-name="Arc"
-      gtk-font-name="Iosevka 11"
-      gtk-cursor-theme-size=0
-      gtk-toolbar-style=GTK_TOOLBAR_BOTH_HORIZ
-      gtk-toolbar-icon-size=GTK_ICON_SIZE_LARGE_TOOLBAR
-      gtk-button-images=0
-      gtk-menu-images=0
-      gtk-enable-event-sounds=1
-      gtk-enable-input-feedback-sounds=1
-      gtk-xft-antialias=1
-      gtk-xft-hinting=1
-      gtk-xft-hintstyle="hintmedium"
-    '';
-  };
-
-  home.file.".config/gtk-2.0/gtkfilechooser.ini" = {
-    text = ''
-      [Filechooser Settings]
-      LocationMode=path-bar
-      ShowHidden=false
-      ShowSizeColumn=true
-      GeometryX=442
-      GeometryY=212
-      GeometryWidth=1036
-      GeometryHeight=609
-      SortColumn=name
-      SortOrder=ascending
-      StartupMode=recent
-    '';
-  };
-
-  home.file.".config/gtk-3.0/settings.ini" = {
-    text = ''
-      [Settings]
-      gtk-theme-name=Nordic-darker
-      gtk-icon-theme-name=Arc
-      gtk-font-name=Iosevka 11
-      gtk-cursor-theme-size=0
-      gtk-toolbar-style=GTK_TOOLBAR_BOTH_HORIZ
-      gtk-toolbar-icon-size=GTK_ICON_SIZE_LARGE_TOOLBAR
-      gtk-button-images=0
-      gtk-menu-images=0
-      gtk-enable-event-sounds=1
-      gtk-enable-input-feedback-sounds=1
-      gtk-xft-antialias=1
-      gtk-xft-hinting=1
-      gtk-xft-hintstyle=hintmedium
-    '';
   };
 
   # hyprlock
@@ -149,7 +93,7 @@ in {
     enable = true;
     settings = {
       preload = "${config.home.homeDirectory}/.config/wallpapers/1330031.jpeg";
-      wallpaper = "monitor,contain:${config.home.homeDirectory}/.config/wallpapers/1330031.jpeg";
+      wallpaper = ",${config.home.homeDirectory}/.config/wallpapers/1330031.jpeg";
     };
   };
 }
