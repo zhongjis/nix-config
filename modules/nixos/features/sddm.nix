18d7dd7e587e166154a497160a96c8eeb71ac42d
zhongjis <zhongjie.x.shen@gmail.com>
2024-11-25 03:38:37 -0700

    udpate sddm theme

--- modules/nixos/features/sddm.nix
+++ modules/nixos/features/sddm.nix
@@ -3,5 +3,18 @@
     enable = true;
     package = pkgs.kdePackages.sddm;
     wayland.enable = true;
+    theme = "catppuccin-mocha";
   };
+
+  environment.systemPackages = [
+    (
+      pkgs.catppuccin-sddm.override {
+        flavor = "mocha";
+        font = "Noto Sans";
+        fontSize = "9";
+        background = "${./wallpaper.png}";
+        loginBackground = true;
+      }
+    )
+  ];
 }
