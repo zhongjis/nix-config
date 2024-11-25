ab3b073e560575454e9bfee21bb82ddc20ab2c20
zhongjis <zhongjie.x.shen@gmail.com>
2024-11-25 02:51:57 -0700

    update

--- modules/home-manager/features/stylix/default.nix
+++ modules/home-manager/features/stylix/default.nix
@@ -5,57 +5,22 @@ in {
 
     image = ./gruvbox-mountain-village.png;
 
-    # fonts = {
-    #   monospace = {
-    #     package = pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];};
-    #     name = "JetBrainsMono Nerd Font Mono";
-    #   };
-    #   sansSerif = {
-    #     package = pkgs.dejavu_fonts;
-    #     name = "DejaVu Sans";
-    #   };
-    #   serif = {
-    #     package = pkgs.dejavu_fonts;
-    #     name = "DejaVu Serif";
-    #   };
+    cursor.package = pkgs.bibata-cursors;
+    cursor.name = "Bibata-Modern-Ice";
 
-    #   sizes = {
-    #     applications = 12;
-    #     terminal = 15;
-    #     desktop = 10;
-    #     popups = 10;
-    #   };
-    # };
-
-    # cursor.name = "Banana-Gruvbox";
-    # cursor.package = pkgs.bibata-cursors;
-
-    # cursor.package = let
-    #   banana = pkgs.stdenv.mkDerivation {
-    #     name = "banana-cursor";
-
-    #     src = builtins.fetchurl {
-    #       url = "https://github.com/vimjoyer/banana-cursor-gruvbox/releases/download/4/Banana-Gruvbox.tar.gz";
-    #       sha256 = "sha256-opGDdW7w2eAhwP/fuBES3qA6d7M8I/xhdXiTXoIoGzs=";
-    #     };
-    #     unpack = false;
-
-    #     installPhase = ''
-    #       mkdir -p $out/share/icons/Banana-Gruvbox
-    #       tar -xvf $src -C $out/share/icons/Banana-Gruvbox
-    #     '';
-    #   };
-    # in
-    #   banana;
-
-    # targets.chromium.enable = true;
-    # targets.grub.enable = true;
-    # targets.grub.useImage = true;
-    # targets.plymouth.enable = true;
-
-    # opacity.terminal = 1;
-    # stylix.targets.nixos-icons.enable = true;
-
-    autoEnable = true;
+    fonts = {
+      monospace = {
+        package = pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];};
+        name = "JetBrainsMono Nerd Font Mono";
+      };
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans";
+      };
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+    };
   };
 }
