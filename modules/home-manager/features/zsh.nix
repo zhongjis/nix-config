008640c4da60eae78f4a79323e1a7b631d46e370
zhongjis <zhongjie.x.shen@gmail.com>
2024-11-25 16:51:50 -0700

    udpate zsh

--- modules/home-manager/features/zsh.nix
+++ modules/home-manager/features/zsh.nix
@@ -1,9 +1,9 @@
 {pkgs, ...}: {
   home.file = {
     ".local/share/zsh/zsh-fast-syntax-highlighting".source = "${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions";
-    ".local/share/zsh/zsh-autocomplete".source = "${pkgs.zsh-autocomplete}/share/zsh-zshautocomplete";
-    ".local/share/zsh/nix-zsh-completions".source = "${pkgs.nix-zsh-completions}/share/zsh/plugins/nix";
     ".local/share/zsh/zsh-vi-mode".source = "${pkgs.zsh-vi-mode}/share/zsh-vi-mode";
+    ".local/share/zsh/nix-zsh-completions".source = "${pkgs.nix-zsh-completions}/share/zsh/plugins/nix";
+    ".local/share/zsh/zsh-autocomple".source = "${pkgs.zsh-autocomplete}/share/zsh-autocomplete";
   };
 
   programs.zsh = {
@@ -34,7 +34,9 @@
         source "$HOME/.local/share/zsh/zsh-vi-mode/zsh-vi-mode.plugin.zsh"
         source "$HOME/.local/share/zsh/zsh-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
         source "$HOME/.local/share/zsh/nix-zsh-completions/nix.plugin.zsh"
-        source "$HOME/.local/share/zsh/zsh-autocompletions/zsh-autocomplete.plugin.zsh"
+        source "$HOME/.local/share/zsh/zsh-autocomple/zsh-autocomplete.plugin.zsh"
+
+        zvm_after_init_commands+=('[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh')
       '';
   };
 }
