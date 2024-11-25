c056da6fe19977369434d81f8fbe8db6d2559e49
zhongjis <zhongjie.x.shen@gmail.com>
2024-11-25 02:25:10 -0700

    change scheme to catppuccin

--- modules/home-manager/features/stylix/default.nix
+++ modules/home-manager/features/stylix/default.nix
@@ -1,23 +1,25 @@
 {pkgs, ...}: {
-  stylix = {
-    base16Scheme = {
-      base00 = "242424"; # ----
-      base01 = "3c3836"; # ---
-      base02 = "504945"; # --
-      base03 = "665c54"; # -
-      base04 = "bdae93"; # +
-      base05 = "d5c4a1"; # ++
-      base06 = "ebdbb2"; # +++
-      base07 = "fbf1c7"; # ++++
-      base08 = "fb4934"; # red
-      base09 = "fe8019"; # orange
-      base0A = "fabd2f"; # yellow
-      base0B = "b8bb26"; # green
-      base0C = "8ec07c"; # aqua/cyan
-      base0D = "7daea3"; # blue
-      base0E = "e089a1"; # purple
-      base0F = "f28534"; # brown
-    };
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+
+  # stylix = {
+  #   base16Scheme = {
+  #     base00 = "242424"; # ----
+  #     base01 = "3c3836"; # ---
+  #     base02 = "504945"; # --
+  #     base03 = "665c54"; # -
+  #     base04 = "bdae93"; # +
+  #     base05 = "d5c4a1"; # ++
+  #     base06 = "ebdbb2"; # +++
+  #     base07 = "fbf1c7"; # ++++
+  #     base08 = "fb4934"; # red
+  #     base09 = "fe8019"; # orange
+  #     base0A = "fabd2f"; # yellow
+  #     base0B = "b8bb26"; # green
+  #     base0C = "8ec07c"; # aqua/cyan
+  #     base0D = "7daea3"; # blue
+  #     base0E = "e089a1"; # purple
+  #     base0F = "f28534"; # brown
+  #   };
 
     image = ./gruvbox-mountain-village.png;
 
