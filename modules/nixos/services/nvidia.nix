60ea1ae08eead5d45c64275456444126becdcbac
zhongjis <zhongjie.x.shen@gmail.com>
2024-11-25 03:35:10 -0700

    update

--- modules/nixos/services/nvidia.nix
+++ modules/nixos/services/nvidia.nix
@@ -5,7 +5,6 @@
   ...
 }: {
   services.xserver.videoDrivers = ["nvidia"];
-  boot.kernelParams = ["nvidia.NVreg_PreserveVideoMemoryAllocations=1"];
 
   hardware.nvidia = {
     modesetting.enable = true;
