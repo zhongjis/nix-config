2f087831a908fe74b658980b121cc9e42cc968d1
zhongjis <zhongjie.x.shen@gmail.com>
2024-11-27 15:01:47 -0700

    tweak rofi toggle
    now show windows also

--- modules/home-manager-linux/features/rofi/rofi-toggle.sh
+++ modules/home-manager-linux/features/rofi/rofi-toggle.sh
@@ -6,7 +6,8 @@ if pgrep -x "rofi" >/dev/null; then
 else
   # Rofi not running, launch it
   # rofi -show combi -modes combi -combi-modes "window,drun,run"
-  rofi -show drun -show-icons
+  rofi -show combi -modes combi -combi-modes "window,drun"
+  # rofi -show drun -show-icons
   sleep 0.2 # Small delay to let Rofi open
   hyprctl dispatch focuswindow active
 fi
