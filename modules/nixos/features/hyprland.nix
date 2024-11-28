42fa663960126df9075a7d32a22f4cb84b403b22
zhongjis <zhongjie.x.shen@gmail.com>
2024-11-28 14:17:07 -0700

    I give up on this

--- modules/nixos/features/hyprland.nix
+++ modules/nixos/features/hyprland.nix
@@ -8,8 +8,6 @@
   programs.uwsm.enable = true;
   programs.hyprland = {
     enable = true;
-    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     xwayland.enable = true;
     withUWSM = true;
   };
@@ -24,16 +22,4 @@
   ];
 
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
-
-  # xdg.portal = {
-  #   enable = true;
-  #   wlr.enable = false;
-  #   extraPortals = [
-  #     pkgs.xdg-desktop-portal-gtk
-  #   ];
-  #   configPackages = [
-  #     pkgs.xdg-desktop-portal-gtk
-  #     pkgs.xdg-desktop-portal
-  #   ];
-  # };
 }
