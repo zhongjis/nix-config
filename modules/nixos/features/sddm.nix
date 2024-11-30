3d07a7c30f00b503b835d37f09e98b10e3905598
zhongjis <zhongjie.x.shen@gmail.com>
2024-11-30 13:35:26 -0700

    disable wayland in sddm

--- modules/nixos/features/sddm.nix
+++ modules/nixos/features/sddm.nix
@@ -7,7 +7,7 @@
   services.displayManager.sddm = {
     enable = true;
     package = lib.mkForce pkgs.kdePackages.sddm;
-    wayland.enable = lib.mkForce true;
+    # wayland.enable = lib.mkForce true;
     theme = lib.mkForce "catppuccin-mocha";
   };
 
