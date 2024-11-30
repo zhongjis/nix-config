cb57b850411cee46d787e1c34e6ba46567bf845c
zhongjis <zhongjie.x.shen@gmail.com>
2024-11-29 21:32:16 -0700

    update power-management

--- modules/nixos/features/power-management.nix
+++ modules/nixos/features/power-management.nix
@@ -4,12 +4,12 @@
 
   # services.power-profiles-daemon.enable = true;
 
-  # powerManagement = {
-  #   enable = true;
-  #   cpuFreqGovernor = "schedutil";
-  # };
+  powerManagement = {
+    enable = true;
+    cpuFreqGovernor = "schedutil";
+  };
 
-  # services.auto-cpufreq.enable = true;
+  services.auto-cpufreq.enable = true;
   # services.auto-cpufreq.settings = {
   #   battery = {
   #     governor = "powersave";
@@ -22,23 +22,23 @@
   # };
 
   # services.thermald.enable = true;
-  services.tlp = {
-    enable = true;
-    settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+  # services.tlp = {
+  #   enable = true;
+  #   settings = {
+  #     CPU_SCALING_GOVERNOR_ON_AC = "performance";
+  #     CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+  #     CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+  #     CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
 
-      CPU_MIN_PERF_ON_AC = 0;
-      CPU_MAX_PERF_ON_AC = 100;
-      CPU_MIN_PERF_ON_BAT = 10;
-      CPU_MAX_PERF_ON_BAT = 30;
+  #     CPU_MIN_PERF_ON_AC = 0;
+  #     CPU_MAX_PERF_ON_AC = 100;
+  #     CPU_MIN_PERF_ON_BAT = 10;
+  #     CPU_MAX_PERF_ON_BAT = 30;
 
-      #Optional helps save long term battery health
-      START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
-      STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
-    };
-  };
+  #     #Optional helps save long term battery health
+  #     START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
+  #     STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+  #   };
+  # };
 }
