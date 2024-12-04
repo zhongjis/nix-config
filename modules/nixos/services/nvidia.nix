2f0e2b70177e298a33156ab4171f0705193bb23b
zhongjis <zhongjie.x.shen@gmail.com>
2024-12-04 11:12:56 -0700

    add new on-the-go spec

--- modules/nixos/services/nvidia.nix
+++ modules/nixos/services/nvidia.nix
@@ -43,6 +43,29 @@
     };
   };
 
+  specialisation = {
+    # disable nvidia gpu for power saving on the go
+    on-the-go.configuration = {
+      boot.extraModprobeConfig = lib.mkForce ''
+        blacklist nouveau
+        options nouveau modeset=0
+      '';
+
+      services.udev.extraRules = lib.mkForce ''
+        # Remove NVIDIA USB xHCI Host Controller devices, if present
+        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+        # Remove NVIDIA USB Type-C UCSI devices, if present
+        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+        # Remove NVIDIA Audio devices, if present
+        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+        # Remove NVIDIA VGA/3D controller devices
+        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+      '';
+
+      boot.blacklistedKernelModules = lib.mkForce ["nouveau" "nvidia" "nvidia_drm" "nvidia_modeset"];
+    };
+  };
+
   environment.systemPackages = with pkgs; [
     pciutils
     # gwe # not supported for wayland yet
