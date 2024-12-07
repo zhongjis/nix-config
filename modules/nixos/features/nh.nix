bddc74419928888009c2b4e14b15efcc8b7729d0
zhongjis <zhongjie.x.shen@gmail.com>
2024-12-07 04:52:22 -0700

    update nh nixos module setting

--- modules/nixos/features/nh.nix
+++ modules/nixos/features/nh.nix
@@ -4,7 +4,7 @@
     enable = true;
     package = pkgs.nh;
     clean.enable = true;
-    clean.extraArgs = "--keep-since 30d --keep 10";
+    clean.extraArgs = "--keep-since 14d --keep 10";
     flake = "/home/zshen/personal/nix-config";
   };
 }
