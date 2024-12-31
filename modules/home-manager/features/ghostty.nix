23fe9b0bc41e3792ed8b35e7e4c32a99a09c1cb9
zhongjis <zhongjie.x.shen@gmail.com>
2024-12-31 08:59:21 -0700

    update ghostty config

--- modules/home-manager/features/ghostty.nix
+++ modules/home-manager/features/ghostty.nix
@@ -1,5 +1,27 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   home.packages = with pkgs; [
     ghostty
   ];
+
+  home.file."${config.xdg.configHome}/ghostty/config" = {
+    text =
+      /*
+      toml
+      */
+      ''
+        font-family = JetBrainsMono Nerd Font
+        font-size = 14
+        theme = catppuccin-mocha
+        window-decoration = false
+        window-padding-x = 18
+        window-padding-y = 12
+        initial-command = zsh -l -c 'tmux attach || tmux new-session -d -s home \"&& tmux attach -t home'
+        confirm-close-surface = false
+        background-opacity = 0.9
+      '';
+  };
 }
