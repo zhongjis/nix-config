d86bc06f898e4af41b5ce73c1bfee2e2a18a7c66
zshen <zhongjie.x.shen@gmail.com>
2025-01-04 12:32:29 -0700

    add fcitx5

---
+++ modules/nixos/features/kb-chinese-layout.nix
@@ -0,0 +1,17 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}: {
+  # reference: https://nixos.wiki/wiki/Fcitx5
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-gtk
+      fcitx5-chinese-addons
+      fcitx5-nord
+    ];
+  };
+}
