e7813d7cc29e3a340921f83abcd75b7248acf2ed
zshen <zhongjie.x.shen@gmail.com>
2025-01-04 13:04:56 -0700

    tweak fcitx5

--- modules/nixos/features/multi-lang-input-layout.nix
+++ modules/nixos/features/multi-lang-input-layout.nix
@@ -8,11 +8,14 @@
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-    fcitx5.addons = with pkgs; [
-      fcitx5-gtk
-      fcitx5-chinese-addons
-      fcitx5-nord
-    ];
+    fcitx5 = {
+      waylandFrontend = true;
+      addons = with pkgs; [
+        fcitx5-gtk
+        fcitx5-chinese-addons
+        fcitx5-nord
+      ];
+    };
   };
 
   # fix Fcitx5 Doesn't Start When Using WM
