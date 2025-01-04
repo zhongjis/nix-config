c33a6a9c6849e7bce8074f1c06aa780142b55832
zshen <zhongjie.x.shen@gmail.com>
2025-01-04 11:23:06 -0700

    fix zen browser hypr workspace

--- modules/home-manager-linux/features/hyprland/default.nix
+++ modules/home-manager-linux/features/hyprland/default.nix
@@ -164,7 +164,7 @@ in {
         windowrulev2 = workspace special:magic,class:^(discord)$
         windowrulev2 = workspace 7,class:^(obsidian)$
         windowrulev2 = workspace 9,class:^(steam)$
-        windowrulev2 = workspace 10,class:^(zen-alpha)$
+        windowrulev2 = workspace 10,class:^(zen-beta)$
         # windowrulev2 = float,center,size 40% 60%,class:kitty,title:btop
         # windowrulev2 = float,center,size 40% 60%,class:kitty,title:nvtop
         # windowrulev2 = float,center,size 40% 60%,class:kitty,title:nmtui
