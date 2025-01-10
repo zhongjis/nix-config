e77d3911378fd7331451a9958c3f6407f1d5a931
zhongjis <zhongjie.x.shen@gmail.com>
2025-01-09 21:37:24 -0700

    fix stylix common issue

--- modules/home-manager/features/stylix/other.nix
+++ modules/home-manager/features/stylix/other.nix
@@ -1,5 +1,17 @@
 {pkgs, ...}: let
 in {
+  # stylix.cursor = {
+  #   name = "Bibata-Modern-Ice";
+  #   package = pkgs.bibata-cursors;
+  #   size = 32;
+  # };
+
+  stylix.cursor = {
+    name = "catppuccin-mocha-light-cursors";
+    package = pkgs.catppuccin-cursors.mochaLight;
+    size = 32;
+  };
+
   stylix.targets = {
     waybar.enable = false;
     neovim.enable = false;
