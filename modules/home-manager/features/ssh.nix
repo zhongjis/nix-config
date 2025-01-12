22b0bfffb4142054eba6673fb406e213402d9b1e
zshen <zhongjie.x.shen@gmail.com>
2025-01-12 10:25:35 -0700

    add optional darwin option

--- modules/home-manager/features/ssh.nix
+++ modules/home-manager/features/ssh.nix
@@ -1,12 +1,26 @@
-{config, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   programs.ssh.enable = true;
   programs.ssh.matchBlocks = {
     "github-com" = {
       host = "github.com";
       identityFile = "${config.home.homeDirectory}/.ssh/github_com_zhongjis";
-      extraOptions = {
-        "PreferredAuthentications" = "publickey";
-      };
+      extraOptions = let
+        baseOptions = {
+          PreferredAuthentications = "publickey";
+          AddKeysToAgent = "yes";
+        };
+        conditionalOptions =
+          if pkgs.stdenv.isLinux
+          then {}
+          else {
+            UseKeychain = "yes";
+          };
+      in
+        baseOptions // conditionalOptions;
     };
   };
 }
