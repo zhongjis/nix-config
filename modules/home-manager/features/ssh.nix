863f14e74e96ea07d5d6daebe9c63a28593e2a49
ZShen <zhongjie.x.shen@gmail.com>
2025-01-12 10:43:26 -0700

    add optional darwin option (#26)
    * add optional darwin option
    
    * update setting
    
    * add git corp
    
    * fix path

--- modules/home-manager/features/ssh.nix
+++ modules/home-manager/features/ssh.nix
@@ -1,12 +1,38 @@
-{config, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: let
+  darwinKeychainOption =
+    if pkgs.stdenv.isDarwin
+    then {
+      UseKeychain = "yes";
+    }
+    else {};
+in {
   programs.ssh.enable = true;
   programs.ssh.matchBlocks = {
     "github-com" = {
       host = "github.com";
       identityFile = "${config.home.homeDirectory}/.ssh/github_com_zhongjis";
-      extraOptions = {
-        "PreferredAuthentications" = "publickey";
-      };
+      extraOptions = let
+        baseOptions = {
+          PreferredAuthentications = "publickey";
+          AddKeysToAgent = "yes";
+        };
+      in
+        baseOptions // darwinKeychainOption;
+    };
+    "git-corp-adobe-com" = {
+      host = "git.corp.adobe.com";
+      identityFile = "${config.home.homeDirectory}/.ssh/github_adobe_zshen";
+      extraOptions = let
+        baseOptions = {
+          PreferredAuthentications = "publickey";
+          AddKeysToAgent = "yes";
+        };
+      in
+        baseOptions // darwinKeychainOption;
     };
   };
 }
