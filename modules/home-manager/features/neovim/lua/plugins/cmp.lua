2ce283abb0488a0529f04c195396aaeba86a7777
zshen <zhongjie.x.shen@gmail.com>
2025-01-19 13:25:57 -0700

    add sleuth and update cmp

--- modules/home-manager/features/neovim/lua/plugins/cmp.lua
+++ modules/home-manager/features/neovim/lua/plugins/cmp.lua
@@ -1,32 +1,4 @@
-local kind_icons = {
-  Text = "",
-  Method = "󰆧",
-  Function = "󰊕",
-  Constructor = "",
-  Field = "󰇽",
-  Variable = "󰂡",
-  Class = "󰠱",
-  Interface = "",
-  Module = "",
-  Property = "󰜢",
-  Unit = "",
-  Value = "󰎠",
-  Enum = "",
-  Keyword = "󰌋",
-  Snippet = "",
-  Color = "󰏘",
-  File = "󰈙",
-  Reference = "",
-  Folder = "󰉋",
-  EnumMember = "",
-  Constant = "󰏿",
-  Struct = "",
-  Event = "",
-  Operator = "󰆕",
-  TypeParameter = "󰅲",
-}
-
-return {
+return { -- Autocompletion
   "hrsh7th/nvim-cmp",
   event = "InsertEnter",
   dependencies = {
@@ -47,16 +19,18 @@ return {
         },
       },
     },
-
     "saadparwaiz1/cmp_luasnip",
+
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-path",
+    "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-cmdline",
   },
   config = function()
+    -- See `:help cmp`
     local cmp = require("cmp")
     local luasnip = require("luasnip")
-    luasnip.config.setup()
+    luasnip.config.setup({})
 
     cmp.setup({
       snippet = {
@@ -64,11 +38,7 @@ return {
           luasnip.lsp_expand(args.body)
         end,
       },
-
-      -- completion = {
-      --   completeopt = "menu,menuone,noinsert",
-      -- },
-
+      completion = { completeopt = "menu,menuone,noinsert" },
       mapping = cmp.mapping.preset.insert({
         ["<C-n>"] = cmp.mapping.select_next_item({
           behavior = cmp.SelectBehavior.Insert,
@@ -85,60 +55,35 @@ return {
         ),
         ["<C-Space>"] = cmp.mapping.complete({}),
       }),
-
-      sources = cmp.config.sources({
+      sources = {
+        {
+          name = "lazydev",
+          group_index = 0,
+        },
+        { name = "buffer" },
         { name = "nvim_lsp" },
         { name = "luasnip" },
         { name = "path" },
-      }, {
-        { name = "buffer" },
-      }),
-
-      formatting = {
-        format = function(entry, vim_item)
-          local lspkind_ok, lspkind = pcall(require, "lspkind")
-          if not lspkind_ok then
-            -- From kind_icons array
-            vim_item.kind =
-              string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind) -- This concatenates the icons with the name of the item kind
-            -- Source
-            vim_item.menu = ({
-              buffer = "[Buffer]",
-              nvim_lsp = "[LSP]",
-              luasnip = "[LuaSnip]",
-              nvim_lua = "[Lua]",
-              latex_symbols = "[LaTeX]",
-            })[entry.source.name]
-            return vim_item
-          else
-            -- From lspkind
-            return lspkind.cmp_format()(entry, vim_item)
-          end
-        end,
       },
     })
 
-    -- `/` cmdline setup.
-    cmp.setup.cmdline("/", {
+    -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline({ "/", "?" }, {
       mapping = cmp.mapping.preset.cmdline(),
       sources = {
         { name = "buffer" },
       },
     })
 
-    -- `:` cmdline setup.
+    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
     cmp.setup.cmdline(":", {
       mapping = cmp.mapping.preset.cmdline(),
       sources = cmp.config.sources({
         { name = "path" },
       }, {
-        {
-          name = "cmdline",
-          option = {
-            ignore_cmds = { "Man", "!" },
-          },
-        },
+        { name = "cmdline" },
       }),
+      matching = { disallow_symbol_nonprefix_matching = false },
     })
   end,
 }
