8883b78a12dd3e3e3298d5a7581009ee166cd0eb
zshen <zhongjie.x.shen@gmail.com>
2025-01-28 01:23:50 -0700

    remove docker group

--- hosts/framework-16/configuration.nix
+++ hosts/framework-16/configuration.nix
@@ -83,7 +83,7 @@
   users.users.zshen = {
     isNormalUser = true;
     description = "Jason";
-    extraGroups = ["networkmanager" "wheel" "audio" "docker"];
+    extraGroups = ["networkmanager" "wheel" "audio"];
     packages = with pkgs; [];
     shell = pkgs.zsh;
   };
