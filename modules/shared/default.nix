2f6b2f82f3b4f79788780cd1b452d2bd2be3c7d7
zhongjis <zhongjie.x.shen@gmail.com>
2025-01-27 23:22:45 -0700

    add nix-community cache

--- modules/shared/default.nix
+++ modules/shared/default.nix
@@ -11,16 +11,18 @@
     trusted-users = ["root" "@wheel" "zshen"];
     substituters = [
       "https://cuda-maintainers.cachix.org"
-      "https://cache.nixos.org/"
+      "https://cache.nixos.org"
       "https://hyprland.cachix.org"
       "https://devenv.cachix.org"
       "https://nix-gaming.cachix.org"
+      "https://nix-community.cachix.org"
     ];
     trusted-public-keys = [
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 
