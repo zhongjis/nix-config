0557a9689ad3f502a22f2a643b4bf76a2a91a1f8
zshen <zhongjie.x.shen@gmail.com>
2025-01-28 22:28:39 -0700

    tweak firewall and update podman config

--- modules/nixos/features/podman.nix
+++ modules/nixos/features/podman.nix
@@ -1,29 +1,22 @@
 {pkgs, ...}: {
   virtualisation.libvirtd.enable = true;
   # Enable common container config files in /etc/containers
-  virtualisation.containers.enable = true;
-  virtualisation = {
-    podman = {
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+    autoPrune = {
       enable = true;
-      dockerCompat = true;
-      autoPrune = {
-        enable = true;
-        dates = "weekly";
-        flags = [
-          "--filter=until=24h"
-          "--filter=label!=important"
-        ];
-      };
-      defaultNetwork.settings.dns_enabled = true;
+      dates = "weekly";
+      flags = [
+        "--filter=until=24h"
+        "--filter=label!=important"
+      ];
     };
+    defaultNetwork.settings.dns_enabled = true;
   };
 
   # Useful other development tools
   environment.systemPackages = with pkgs; [
-    dive
-    podman-tui
     podman-compose
   ];
-
-  virtualisation.oci-containers.backend = "podman";
 }
