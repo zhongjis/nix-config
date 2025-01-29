f3027225e03ee2b8df9c3807020afffd83859f4f
zhongjis <zhongjie.x.shen@gmail.com>
2025-01-29 11:19:59 -0700

    update cache

--- modules/shared/default.nix
+++ modules/shared/default.nix
@@ -16,6 +16,7 @@
       "https://devenv.cachix.org"
       "https://nix-gaming.cachix.org"
       "https://nix-community.cachix.org"
+      "https://mitchellh-nixos-config.cachix.org"
     ];
     trusted-public-keys = [
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
@@ -23,6 +24,7 @@
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "mitchellh-nixos-config.cachix.org-1:bjEbXJyLrL1HZZHBbO4QALnI5faYZppzkU4D2s0G8RQ="
     ];
   };
 
