87f757f40252cdc410762c0b77562d53deb91cf4
zhongjis <zhongjie.x.shen@gmail.com>
2025-01-30 12:15:46 -0700

    refactor flake input format

--- flake.nix
+++ flake.nix
@@ -26,22 +26,14 @@
 
     ## nix-homebrew
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
-    homebrew-core = {
-      url = "github:homebrew/homebrew-core";
-      flake = false;
-    };
-    homebrew-cask = {
-      url = "github:homebrew/homebrew-cask";
-      flake = false;
-    };
-    homebrew-bundle = {
-      url = "github:homebrew/homebrew-bundle";
-      flake = false;
-    };
-    aerospace-tap = {
-      url = "github:nikitabobko/homebrew-tap";
-      flake = false;
-    };
+    homebrew-core.url = "github:homebrew/homebrew-core";
+    homebrew-core.flake = false;
+    homebrew-cask.url = "github:homebrew/homebrew-cask";
+    homebrew-cask.flake = false;
+    homebrew-bundle.url = "github:homebrew/homebrew-bundle";
+    homebrew-bundle.flake = false;
+    aerospace-tap.url = "github:nikitabobko/homebrew-tap";
+    aerospace-tap.flake = false;
 
     # programs
     xremap-flake.url = "github:xremap/nix-flake";
