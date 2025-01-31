9145bba52853f98c48f42b6a55205e1a1dcb3864
zshen <zhongjie.x.shen@gmail.com>
2025-01-31 01:46:02 -0700

    rename module

--- hosts/framework-16/home.nix
+++ hosts/framework-16/home.nix
@@ -60,7 +60,7 @@ in {
   myHomeManager.bundles.hyprland.enable = true;
   myHomeManager.bundles.office.enable = true;
 
-  myHomeManager.podman-containers.enable = false;
+  myHomeManager.llm.enable = false;
 
   programs.git.userName = "zhongjis";
   programs.git.userEmail = "zhongjie.x.shen@gmail.com";
