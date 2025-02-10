aca508a48024db7ea18e7f7b95ace97d59f522c0
zhongjis <zhongjie.x.shen@gmail.com>
2025-02-10 06:19:24 -0700

    update fzf setting

--- modules/shared/home-manager/features/fzf.nix
+++ modules/shared/home-manager/features/fzf.nix
@@ -2,7 +2,14 @@
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
-    changeDirWidgetCommand = "fd --type -d";
+
+    defaultCommand = "fd --type f";
+    defaultOptions = [
+      "--height 40%"
+      "--prompt ⟫"
+    ];
+
+    changeDirWidgetCommand = "fd --type d";
     changeDirWidgetOptions = [
       "--preview 'tree -C {} | head -200'"
     ];
