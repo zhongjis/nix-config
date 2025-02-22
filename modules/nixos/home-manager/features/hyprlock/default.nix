2b41d95ea0f0c51b6440c0e1c14ca6f8ad2c4b51
zshen <zhongjie.x.shen@gmail.com>
2025-02-22 16:13:41 -0700

    update hyprlock

--- modules/nixos/home-manager/features/hyprlock/default.nix
+++ modules/nixos/home-manager/features/hyprlock/default.nix
@@ -13,81 +13,82 @@ in {
     hyprlang
     */
       ''
+        background {
+            monitor =
+            path = ${config.stylix.image}
+            blur_passes = 2
+            contrast = 1
+            brightness = 0.5
+            vibrancy = 0.2
+            vibrancy_darkness = 0.2
+        }
+
         general {
-            grace = 1
+            no_fade_in = true
+            no_fade_out = true
+            hide_cursor = false
+            grace = 0
+            disable_loading_bar = true
         }
 
         fingerprint {
           enabled = true
         }
 
-        background {
-            monitor =
-            path = ${config.stylix.image}
-            blur_size = 5
-            blur_passes = 1 # 0 disables blurring
-            noise = 0.0117
-            contrast = 1.3000 # Vibrant!!!
-            brightness = 0.8000
-            vibrancy = 0.2100
-            vibrancy_darkness = 0.0
-        }
-
+        # INPUT FIELD
         input-field {
             monitor =
-            size = 200, 50
-            outline_thickness = 3
-            dots_size = 0.33 # Scale of input-field height, 0.2 - 0.8
-            dots_spacing = 0.15 # Scale of dots' absolute size, 0.0 - 1.0
+            size = 250, 60
+            outline_thickness = 2
+            dots_size = 0.2 # Scale of input-field height, 0.2 - 0.8
+            dots_spacing = 0.35 # Scale of dots' absolute size, 0.0 - 1.0
             dots_center = true
-            dots_rounding = -1 # -1 default circle, -2 follow input-field rounding
-            outer_color=rgb(${base0A})
-            inner_color=rgb(${base08})
-            font_color=rgb(${base05})
-            fade_on_empty = true
-            fade_timeout = 1000 # Milliseconds before fade_on_empty is triggered.
-            placeholder_text = <i>Input Password...</i> # Text rendered in the input box when it's empty.
+            outer_color = rgba(0, 0, 0, 0)
+            inner_color = rgba(0, 0, 0, 0.2)
+            font_color = $foreground
+            fade_on_empty = false
+            rounding = -1
+            check_color = rgb(204, 136, 34)
+            placeholder_text = <i><span foreground="##cdd6f4">Input Password...</span></i>
             hide_input = false
-            rounding = -1 # -1 means complete rounding (circle/oval)
-            check_color=rgb(${base03})
-            fail_color=rgb(${base00})
-            fail_text = <i>$FAIL <b>($ATTEMPTS)</b></i> # can be set to empty
-            fail_transition = 300 # transition time in ms between normal outer_color and fail_color
-            capslock_color = -1
-            numlock_color = -1
-            bothlock_color = -1 # when both locks are active. -1 means don't change outer color (same for above)
-            invert_numlock = false # change color if numlock is off
-            swap_font_color = false # see below
-            position = 0, -20
+            position = 0, -200
             halign = center
             valign = center
         }
 
+        # DATE
         label {
-            monitor =
-            #clock
-            text = cmd[update:1000] echo "$TIME"
-            color = rgba(200, 200, 200, 1.0)
-            font_size = 55
-            font_family = Fira Semibold
-            position = -100, 70
-            halign = right
-            valign = bottom
-            shadow_passes = 5
-            shadow_size = 10
+          monitor =
+          text = cmd[update:1000] echo "$(date +"%A, %B %d")"
+          color = rgba(242, 243, 244, 0.75)
+          font_size = 22
+          font_family = JetBrains Mono
+          position = 0, 300
+          halign = center
+          valign = center
+        }
+
+        # TIME
+        label {
+          monitor =
+          text = cmd[update:1000] echo "$(date +"%-I:%M")"
+          color = rgba(242, 243, 244, 0.75)
+          font_size = 95
+          font_family = JetBrains Mono Extrabold
+          position = 0, 200
+          halign = center
+          valign = center
         }
 
         label {
             monitor =
-            text = $USER
-            color = rgba(200, 200, 200, 1.0)
-            font_size = 20
-            font_family = Fira Semibold
-            position = -100, 160
+            text = cmd[update:1000] echo "$(${pkgs.acpi}/bin/acpi -- -b | cut -d ',' -f1,2)"
+            color = $foreground
+            font_size = 24
+            font_family = JetBrains Mono
+            position = -90, -10
             halign = right
-            valign = bottom
-            shadow_passes = 5
-            shadow_size = 10
+            valign = top
         }
       '';
   };
