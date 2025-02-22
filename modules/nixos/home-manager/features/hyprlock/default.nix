5ea6feb79a21bc1402192fea89b4a3d45bc0cb40
zshen <zhongjie.x.shen@gmail.com>
2025-02-22 16:50:21 -0700

    update hyprlock style

--- modules/nixos/home-manager/features/hyprlock/default.nix
+++ modules/nixos/home-manager/features/hyprlock/default.nix
@@ -31,8 +31,11 @@ in {
             disable_loading_bar = true
         }
 
-        fingerprint {
-          enabled = true
+        auth {
+            pam:enabled = true
+            fingerprint:enabled = true
+            fingerprint:ready_message = Place your finger on the sensor
+            fingerprint:present_message = Scanning finger
         }
 
         # INPUT FIELD
@@ -40,17 +43,25 @@ in {
             monitor =
             size = 250, 60
             outline_thickness = 2
+
             dots_size = 0.2 # Scale of input-field height, 0.2 - 0.8
             dots_spacing = 0.35 # Scale of dots' absolute size, 0.0 - 1.0
             dots_center = true
-            outer_color = rgba(0, 0, 0, 0)
-            inner_color = rgba(0, 0, 0, 0.2)
-            font_color = $foreground
-            fade_on_empty = false
+
+            outer_color=rgb(${base0A})
+            inner_color=rgb(${base02})
+
+            font_color=rgb(${base05})
+            fade_on_empty = true
             rounding = -1
-            check_color = rgb(204, 136, 34)
-            placeholder_text = <i><span foreground="##cdd6f4">Input Password...</span></i>
+
+            check_color=rgb(${base0A})
+            fail_color=rgb(${base08})
+            fail_text = <i>$FAIL <b>($ATTEMPTS)</b></i> # can be set to empty
+
+            fail_transition = 300 # transition time in ms between normal outer_color and fail_color
             hide_input = false
+
             position = 0, -200
             halign = center
             valign = center
