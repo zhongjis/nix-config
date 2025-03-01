ab7982525bd58c39cc7bb14628b8e9a014c3e5ba
zshen <zhongjie.x.shen@gmail.com>
2025-03-01 14:58:04 -0700

    update ghostty

--- modules/shared/home-manager/features/ghostty.nix
+++ modules/shared/home-manager/features/ghostty.nix
@@ -25,12 +25,24 @@
       toml
       */
       ''
+        # Cursor
+        cursor-style = block
+        cursor-style-blink = false
+
+        # font
         font-family = JetBrainsMono Nerd Font
         font-size = 14
+
+        # theme
         theme = catppuccin-mocha
+
+        # window
         window-decoration = false
         window-padding-x = 18
         window-padding-y = 12
+        title = "Ghostty"
+
+        # init
         initial-command = zsh -l -c 'tmux attach || tmux new-session -d -s home "fastfetch; exec $SHELL" && tmux attach -t home'
         confirm-close-surface = false
         background-opacity = 0.9
