1abd19018582619af70f49c22305d76dd379a569
zshen <zhongjie.x.shen@gmail.com>
2025-03-09 14:51:35 -0600

    add general server hm bundle

--- hosts/vultr-lab/home.nix
+++ hosts/vultr-lab/home.nix
@@ -8,6 +8,8 @@ in {
   programs.git.userName = "zhongjis";
   programs.git.userEmail = "zhongjie.x.shen@gmail.com";
 
+  myHomeManager.bundles.general-server.enable = true;
+
   home.username = "zshen";
   home.homeDirectory = "/home/zshen";
   home.stateVersion = "23.11";
