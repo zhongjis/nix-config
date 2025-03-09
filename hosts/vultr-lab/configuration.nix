f95bc85ba8ab6da256da49c31fdc21f67c5fa739
zshen <zhongjie.x.shen@gmail.com>
2025-03-09 14:02:36 -0600

    update configuration

--- hosts/vultr-lab/configuration.nix
+++ hosts/vultr-lab/configuration.nix
@@ -7,8 +7,14 @@
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
     (modulesPath + "/profiles/qemu-guest.nix")
+    ../../modules/shared
+    ../../modules/nixos
     ./disk-config.nix
   ];
+
+  myNixOS.sops.enable = true;
+  myNixOS.nh.enable = true;
+
   boot.loader.grub = {
     # no need to set devices, disko will add all devices that have a EF02 partition to the list already
     # devices = [ ];
@@ -19,11 +25,10 @@
 
   environment.systemPackages = map lib.lowPrio [
     pkgs.curl
-    pkgs.gitMinimal
+    pkgs.git
     pkgs.neovim
   ];
 
-  users.users.root.initialPassword = "1234";
   users.users.root.openssh.authorizedKeys.keys = [
     # change this to your ssh key
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHzczNq8Vc/nrjB1pzIhE2+N/O9kEu+naEhD4BAEjokg zhongjie.x.shen@gmail.com"
