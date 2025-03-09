4102a0a7b7b0b7a87ef678cf6eaa7c37afb93062
zshen <zhongjie.x.shen@gmail.com>
2025-03-09 15:04:15 -0600

    cleanup home packages

--- modules/shared/home-manager/bundles/general-server.nix
+++ modules/shared/home-manager/bundles/general-server.nix
@@ -11,7 +11,7 @@
   };
 
   myHomeManager.neovim.enable = lib.mkDefault true;
-  myHomeManager.direnv.enable = lib.mkDefault true;
+  myHomeManager.direnv.enable = lib.mkDefault false;
   myHomeManager.fzf.enable = lib.mkDefault true;
   myHomeManager.git.enable = lib.mkDefault true;
   myHomeManager.k9s.enable = lib.mkDefault true;
@@ -19,11 +19,8 @@
   myHomeManager.lazydocker.enable = lib.mkDefault true;
   myHomeManager.sops.enable = lib.mkDefault true;
   myHomeManager.ssh.enable = lib.mkDefault true;
-  myHomeManager.yazi.enable = lib.mkDefault true;
   myHomeManager.zsh.enable = lib.mkDefault true;
   myHomeManager.starship.enable = lib.mkDefault true;
-
-  programs.btop.enable = true;
 
   home.sessionVariables = {
     FLAKE = "${config.home.homeDirectory}/personal/nix-config";
