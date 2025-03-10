717a1a8009dbc4b40147a15e102f94ee071eea98
zshen <zhongjie.x.shen@gmail.com>
2025-03-10 01:09:05 -0600

    fix typo

--- hosts/vultr-lab/extras.nix
+++ hosts/vultr-lab/extras.nix
@@ -8,19 +8,18 @@
   services.nginx = {
     enable = true;
     recommendedTlsSettings = true;
-    virtualHosts = {
-      demo = {
-        default = true;
-        enableACME = true;
-        forceSSL = true;
-        root = (
-          builtins.fetchTarball {
-            url = "https://storage.mynixos.com/5/resources/96c8eb7b-758d-4c66-842d-c1ce63bb7f93/site.tar.gz";
-            sha256 = "ublnwAsRKl+Sz7XJXE/38el89m3EDPSld4qMwcxqKdo=";
-          }
-        );
-        serverName = "zshen.art";
-      };
+    virtualHosts."zshen.art" = {
+      serverName = "zshen.art";
+
+      default = true;
+      enableACME = true;
+      forceSSL = true;
+      root = (
+        builtins.fetchTarball {
+          url = "https://storage.mynixos.com/5/resources/96c8eb7b-758d-4c66-842d-c1ce63bb7f93/site.tar.gz";
+          sha256 = "ublnwAsRKl+Sz7XJXE/38el89m3EDPSld4qMwcxqKdo=";
+        }
+      );
 
       locations = {
         "/freshrss" = {
