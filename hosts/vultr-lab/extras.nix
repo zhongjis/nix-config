4fb33a80f1adefb3145d3b93011f1d238f5c6f38
zshen <zhongjie.x.shen@gmail.com>
2025-03-10 00:51:23 -0600

    add placeholder

--- hosts/vultr-lab/extras.nix
+++ hosts/vultr-lab/extras.nix
@@ -1,5 +1,26 @@
 {...}: {
+  security.acme = {
+    acceptTerms = true;
+    defaults = {
+      email = "zhongjie.x.shen@gmail.com";
+    };
+  };
   services.nginx = {
     enable = true;
+    recommendedTlsSettings = true;
+    virtualHosts = {
+      demo = {
+        default = true;
+        enableACME = true;
+        forceSSL = true;
+        root = (
+          builtins.fetchTarball {
+            url = "https://storage.mynixos.com/5/resources/96c8eb7b-758d-4c66-842d-c1ce63bb7f93/site.tar.gz";
+            sha256 = "ublnwAsRKl+Sz7XJXE/38el89m3EDPSld4qMwcxqKdo=";
+          }
+        );
+        serverName = "zshen.art";
+      };
+    };
   };
 }
