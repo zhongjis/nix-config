73993460b559b62784ad7a9a016ce40e17085137
zhongjis <zhongjie.x.shen@gmail.com>
2025-03-14 02:27:51 +0000

    at least it works

--- hosts/vultr-lab/extras.nix
+++ hosts/vultr-lab/extras.nix
@@ -14,25 +14,19 @@
       default = true;
       enableACME = true;
       forceSSL = true;
-      root = (
-        builtins.fetchTarball {
-          url = "https://storage.mynixos.com/5/resources/96c8eb7b-758d-4c66-842d-c1ce63bb7f93/site.tar.gz";
-          sha256 = "ublnwAsRKl+Sz7XJXE/38el89m3EDPSld4qMwcxqKdo=";
-        }
-      );
-
-      # locations = {
-      #   "/freshrss" = {
-      #     root = "/var/lib/freshrss";
-      #   };
-      # };
+      # root = (
+      #   builtins.fetchTarball {
+      #     url = "https://storage.mynixos.com/5/resources/96c8eb7b-758d-4c66-842d-c1ce63bb7f93/site.tar.gz";
+      #     sha256 = "ublnwAsRKl+Sz7XJXE/38el89m3EDPSld4qMwcxqKdo=";
+      #   }
+      # );
     };
   };
 
   services.freshrss = {
     enable = true;
     passwordFile = config.sops.secrets."freshrss/default-user-password".path;
-    # virtualHost = "freshrss.zshen.art";
+    virtualHost = "zshen.art";
     baseUrl = "https://zshen.art";
 
     database = {
