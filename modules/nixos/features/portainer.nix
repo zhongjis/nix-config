d769f02925248f7d83e50fd9da8bb024ed7856a0
zshen <zhongjie.x.shen@gmail.com>
2025-03-14 17:48:42 -0600

    add portainer config

--- modules/nixos/features/portainer.nix
+++ modules/nixos/features/portainer.nix
@@ -1,21 +1,11 @@
 {config, ...}: let
-  cfg = config.services.homepage-dashboard;
+  cfg = config.services.portainer;
 in {
-  services.homepage-dashboard = {
-    enable = true;
-  };
   services.portainer = {
-    enable = true; # Default false
-
-    version = "latest"; # Default latest, you can check dockerhub for
-    # other tags.
-
-    openFirewall = "false"; # Default false, set to 'true' if you want
-    # to be able to access via the port on
-    # something other than localhost.
-
-    port = 9443; # Sets the port number in both the firewall and
-    # the docker container port mapping itself.
+    enable = true; 
+    version = "latest"; 
+    openFirewall = "false"; 
+    port = 9443;
   };
 
   services.nginx = {
@@ -24,7 +14,7 @@ in {
       forceSSL = true;
 
       locations."/" = {
-        proxyPass = "http://localhost:8082";
+        proxyPass = "http://localhost:${cfg.port}";
         recommendedProxySettings = true;
       };
     };
