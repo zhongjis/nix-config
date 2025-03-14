276055ad86016175f6a93c54cc7608f2e0c166dd
zhongjis <zhongjie.x.shen@gmail.com>
2025-03-14 22:14:44 +0000

    update readme.md

--- modules/nixos/features/dify.nix
+++ modules/nixos/features/dify.nix
@@ -1,5 +1,11 @@
 {...}: let
 in {
+  # NOTE: this module is depending on dify docker compose file
+  # make sure to expose dify's nginx port by setting
+  # EXPOSE_NGINX_PORT=8080
+  # EXPOSE_NGINX_SSL_PORT=8443
+  # in its .env file
+
   services.nginx = {
     virtualHosts."dify.zshen.me" = {
       serverName = "dify.zshen.me";
