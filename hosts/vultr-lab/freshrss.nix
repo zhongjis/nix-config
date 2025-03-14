2dbf94a0be593c95e94bca151872d1bb1f991fce
zshen <zhongjie.x.shen@gmail.com>
2025-03-14 00:25:54 -0600

    update fresh rss config

--- hosts/vultr-lab/freshrss.nix
+++ hosts/vultr-lab/freshrss.nix
@@ -1,13 +1,21 @@
-{config, ...}: let
+{
+  config,
+  lib,
+  ...
+}: let
   sopsFile = ../../secrets/freshrss.yaml;
+  cfg = config.services.freshrss;
+  user = "freshrss";
 in {
   sops.secrets = {
     # github - personal
     freshrss_user_password = {
       inherit sopsFile;
+      owner = lib.mkIf cfg.enable user;
     };
     freshrss_db_password = {
       inherit sopsFile;
+      owner = lib.mkIf cfg.enable user;
     };
   };
 
@@ -26,7 +34,7 @@ in {
     passwordFile = config.sops.secrets.freshrss_user_password.path;
     virtualHost = "rss.zshen.me";
     baseUrl = "https://rss.zshen.me";
-    user = "freshrss";
+    inherit user;
 
     database = {
       type = "sqlite";
