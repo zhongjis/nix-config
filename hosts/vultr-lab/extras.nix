c91e046d99c1158af244719ea9de1f9d74fa5879
zhongjis <zhongjie.x.shen@gmail.com>
2025-03-14 03:44:52 +0000

    migrate to zshen.me

--- hosts/vultr-lab/extras.nix
+++ hosts/vultr-lab/extras.nix
@@ -14,20 +14,28 @@
       default = true;
       enableACME = true;
       forceSSL = true;
-      # root = (
-      #   builtins.fetchTarball {
-      #     url = "https://storage.mynixos.com/5/resources/96c8eb7b-758d-4c66-842d-c1ce63bb7f93/site.tar.gz";
-      #     sha256 = "ublnwAsRKl+Sz7XJXE/38el89m3EDPSld4qMwcxqKdo=";
-      #   }
-      # );
+      root = (
+        builtins.fetchTarball {
+          url = "https://storage.mynixos.com/5/resources/96c8eb7b-758d-4c66-842d-c1ce63bb7f93/site.tar.gz";
+          sha256 = "ublnwAsRKl+Sz7XJXE/38el89m3EDPSld4qMwcxqKdo=";
+        }
+      );
+    };
+
+    virtualHosts."rss.zshen.me" = {
+      serverName = "rss.zshen.me";
+
+      default = false;
+      enableACME = true;
+      forceSSL = true;
     };
   };
 
   services.freshrss = {
     enable = true;
     passwordFile = config.sops.secrets."freshrss/default-user-password".path;
-    virtualHost = "zshen.art";
-    baseUrl = "https://zshen.art";
+    virtualHost = "rss.zshen.me";
+    baseUrl = "https://rss.zshen.me";
 
     database = {
       type = "pgsql";
