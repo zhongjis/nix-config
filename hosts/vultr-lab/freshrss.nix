280be9d562257d1fcd15413ba17c5e77d59a6a82
zhongjis <zhongjie.x.shen@gmail.com>
2025-03-14 06:32:44 +0000

    update

--- hosts/vultr-lab/freshrss.nix
+++ hosts/vultr-lab/freshrss.nix
@@ -9,38 +9,31 @@
 in {
   sops.secrets = {
     # github - personal
-    freshrss_user_password = {
+    freshrss_password = {
       inherit sopsFile;
       owner = lib.mkIf cfg.enable user;
-    };
-    freshrss_db_password = {
-      inherit sopsFile;
-      owner = lib.mkIf cfg.enable user;
-    };
-  };
-
-  services.nginx = {
-    virtualHosts."rss.zshen.me" = {
-      serverName = "rss.zshen.me";
-
-      default = false;
-      enableACME = true;
-      forceSSL = true;
     };
   };
 
   services.freshrss = {
     enable = true;
-    passwordFile = config.sops.secrets.freshrss_user_password.path;
+    passwordFile = config.sops.secrets.freshrss_password.path;
     virtualHost = "rss.zshen.me";
     baseUrl = "https://${cfg.virtualHost}";
     inherit user;
-
     database = {
       type = "sqlite";
-      passFile = config.sops.secrets.freshrss_db_password.path;
+      passFile = config.sops.secrets.freshrss_password.path;
     };
 
     extensions = [];
+  };
+
+  services.nginx = {
+    virtualHosts."rss.zshen.me" = {
+      serverName = "rss.zshen.me";
+      enableACME = true;
+      forceSSL = true;
+    };
   };
 }
