46f44b1a0d758a2b23d49e240e1b9552189cbbea
zhongjis <zhongjie.x.shen@gmail.com>
2025-03-15 20:54:07 +0000

    fix loki config

--- modules/nixos/features/monitoring.nix
+++ modules/nixos/features/monitoring.nix
@@ -40,33 +40,26 @@
   services.loki = {
     enable = true;
     configuration = {
-      server.http_listen_port = 3030;
       auth_enabled = false;
 
-      ingester = {
-        lifecycler = {
-          address = "127.0.0.1";
-          ring = {
-            kvstore = {
-              store = "inmemory";
-            };
-            replication_factor = 1;
-          };
+      server.http_listen_port = 3030;
+
+      common = {
+        ring = {
+          instance_addr = "127.0.0.1";
+          kvstore.store = "inmemory";
         };
-        chunk_idle_period = "1h";
-        max_chunk_age = "1h";
-        chunk_target_size = 999999;
-        chunk_retain_period = "30s";
-        max_transfer_retries = 0;
+        replication_factor = 1;
+        path_prefix = "/var/lib/loki";
       };
 
       schema_config = {
         configs = [
           {
-            from = "2022-06-06";
-            store = "boltdb-shipper";
+            from = "2020-05-15";
+            store = "tsdb";
             object_store = "filesystem";
-            schema = "v11";
+            schema = "v13";
             index = {
               prefix = "index_";
               period = "24h";
@@ -76,43 +69,11 @@
       };
 
       storage_config = {
-        boltdb_shipper = {
-          active_index_directory = "/var/lib/loki/boltdb-shipper-active";
-          cache_location = "/var/lib/loki/boltdb-shipper-cache";
-          cache_ttl = "24h";
-          shared_store = "filesystem";
-        };
-
         filesystem = {
           directory = "/var/lib/loki/chunks";
         };
       };
-
-      limits_config = {
-        reject_old_samples = true;
-        reject_old_samples_max_age = "168h";
-      };
-
-      chunk_store_config = {
-        max_look_back_period = "0s";
-      };
-
-      table_manager = {
-        retention_deletes_enabled = false;
-        retention_period = "0s";
-      };
-
-      compactor = {
-        working_directory = "/var/lib/loki";
-        shared_store = "filesystem";
-        compactor_ring = {
-          kvstore = {
-            store = "inmemory";
-          };
-        };
-      };
     };
-    # user, group, dataDir, extraFlags, (configFile)
   };
 
   # promtail: port 3031 (8031)
