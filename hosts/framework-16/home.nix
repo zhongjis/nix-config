ad003409bdc90e6556c7f89d59b8843ad22848dc
zshen <zhongjie.x.shen@gmail.com>
2025-03-14 18:10:26 -0600

    remove podman hm config

--- hosts/framework-16/home.nix
+++ hosts/framework-16/home.nix
@@ -60,8 +60,6 @@ in {
   myHomeManager.bundles.hyprland.enable = true;
   myHomeManager.bundles.office.enable = true;
 
-  myHomeManager.podman.enable = false;
-
   programs.git.userName = "zhongjis";
   programs.git.userEmail = "zhongjie.x.shen@gmail.com";
 
