9496c69099ab94dcf9808fb4aa887edcfa60aa4c
zhongjis <zhongjie.x.shen@gmail.com>
2025-03-16 07:46:07 +0000

    wip

--- modules/nixos/features/monitoring.nix
+++ modules/nixos/features/monitoring.nix
@@ -178,7 +178,7 @@
       forceSSL = true;
 
       locations."/" = {
-        proxyPass = "http://127.0.0.1:${config.services.grafana.settings.server.http_port}";
+        proxyPass = "http://127.0.0.1:${toString config.services.grafana.settings.server.http_port}";
         proxyWebsockets = true;
       };
     };
