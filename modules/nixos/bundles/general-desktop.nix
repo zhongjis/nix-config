b2ed5e942a5a6fda41a35dc8802d57dd999b288e
zshen <zhongjie.x.shen@gmail.com>
2025-03-28 22:36:54 -0600

    add kubernetes config

--- modules/nixos/bundles/general-desktop.nix
+++ modules/nixos/bundles/general-desktop.nix
@@ -12,6 +12,7 @@
   myNixOS.xremap.enable = lib.mkDefault true;
   myNixOS.podman.enable = lib.mkDefault true;
   myNixOS.docker.enable = lib.mkDefault false;
+  myNixOS.kubernetes.enable = lib.mkDefault true;
 
   # fwupd - firmware update
   services.fwupd.enable = lib.mkDefault true;
