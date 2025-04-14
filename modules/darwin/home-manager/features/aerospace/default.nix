efb949cf54c0806551b86358022c6ba5609f3bea
zhongjis <zhongjie.x.shen@gmail.com>
2025-04-14 01:43:01 -0600

    update aerosapce

--- modules/darwin/home-manager/features/aerospace/default.nix
+++ modules/darwin/home-manager/features/aerospace/default.nix
@@ -1,3 +1,8 @@
 {...}: {
+  programs.aerospace = {
+    enable = true;
+    package = null; # managed by darwin homebrew
+  };
+
   home.file.".config/aerospace/aerospace.toml".source = ./aerospace.toml;
 }
