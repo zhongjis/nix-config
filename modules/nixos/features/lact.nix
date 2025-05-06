040324a79d9c37997a54a8501b9c7ed876645417
zshen <zhongjie.x.shen@gmail.com>
2025-05-05 22:30:07 -0600

    add lact

---
+++ modules/nixos/features/lact.nix
@@ -0,0 +1,15 @@
+{pkgs, ...}: {
+  environment.systemPackages = with pkgs; [
+    lact
+  ];
+
+  systemd.services.lact = {
+    description = "AMDGPU Control Daemon";
+    after = ["multi-user.target"];
+    wantedBy = ["multi-user.target"];
+    serviceConfig = {
+      ExecStart = "${pkgs.lact}/bin/lact daemon";
+    };
+    enable = true;
+  };
+}
