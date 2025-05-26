78655ea7b2dbbcfd6ff776326041d34a94cfdc63
zshen <zhongjie.x.shen@gmail.com>
2025-05-25 21:24:11 -0600

    revert kernel

--- hosts/framework-16/configuration.nix
+++ hosts/framework-16/configuration.nix
@@ -16,7 +16,7 @@
   ];
 
   # gaming kernel. not sure if it is good
-  boot.kernelPackages = pkgs.linuxPackages_zen;
+  boot.kernelPackages = pkgs.stable.linuxPackages_zen;
 
   myNixOS = {
     bundles.general-desktop.enable = true;
