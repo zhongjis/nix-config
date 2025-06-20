6e57cc44863b79e17ed71e25eb4e19283dce14fe
zshen <zhongjie.x.shen@gmail.com>
2025-06-19 21:39:18 -0600

    migrate symbol

--- modules/nixos/bundles/gnome.nix
+++ modules/nixos/bundles/gnome.nix
@@ -5,7 +5,7 @@
 }: {
   # replacing default hyprland sddm with gdm
   myNixOS.sddm.enable = lib.mkForce false;
-  services.xserver.displayManager.gdm.enable = true;
+  services.displayManager.gdm.enable = true;
 
   # enable gnome
   services.xserver.desktopManager.gnome.enable = true;
--- modules/nixos/bundles/kde.nix
+++ modules/nixos/bundles/kde.nix
@@ -5,7 +5,7 @@
 }: {
   # replacing default hyprland sddm with gdm
   myNixOS.sddm.enable = lib.mkForce true;
-  services.xserver.displayManager.gdm.enable = lib.mkForce false;
+  services.displayManager.gdm.enable = lib.mkForce false;
 
   myNixOS.kde.enable = true;
 }
