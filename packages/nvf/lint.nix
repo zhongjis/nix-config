6aebfee86d4f7501c69efe29bc50068e8300498c
zshen <zhongjie.x.shen@gmail.com>
2025-07-11 14:56:07 -0600

    [nvf] cmp cont.

--- packages/nvf/lint.nix
+++ packages/nvf/lint.nix
@@ -32,9 +32,21 @@
         _type = "lua-inline";
         expr = ''
           function()
-            if vim.opt_local.modifiable:get() then
-              lint.try_lint()
-            end
+            local lint = require("lint")
+            lint.linters_by_ft = {
+              markdown = { "markdownlint" },
+              terraform = { "tflint", "tfsec" },
+            }
+
+            local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+            vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+              group = lint_augroup,
+              callback = function()
+                if vim.opt_local.modifiable:get() then
+                  lint.try_lint()
+                end
+              end
+            })
           end
         '';
       };
