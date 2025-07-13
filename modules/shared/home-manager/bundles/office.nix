f3ce06f2a43b643659908b4264df1392bd1b3fb0
zshen <zhongjie.x.shen@gmail.com>
2025-07-13 12:22:38 -0600

    [nextcloud] use stable client

--- modules/shared/home-manager/bundles/office.nix
+++ modules/shared/home-manager/bundles/office.nix
@@ -1,9 +1,12 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    nextcloud-client
-    # libreoffice
-    libreoffice
-    hunspell
-    hunspellDicts.en_US-large
-  ];
+{pkgs, ...}: let
+  stable_pkgs = with pkgs.stable; [nextcloud-client];
+in {
+  home.packages = with pkgs;
+    [
+      # libreoffice
+      libreoffice
+      hunspell
+      hunspellDicts.en_US-large
+    ]
+    ++ stable_pkgs;
 }
