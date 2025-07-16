4d0dfd8300b17601601c1b8826e1294e9c92aa06
zshen <zhongjie.x.shen@gmail.com>
2025-07-15 20:17:32 -0600

    [opencode] fix

--- modules/shared/home-manager/features/opencode.nix
+++ modules/shared/home-manager/features/opencode.nix
@@ -1,4 +1,8 @@
-{config, ...}: let
+{
+  config,
+  pkgs,
+  ...
+}: let
   sopsFile = ../../../../secrets/ai-tokens.yaml;
 in {
   sops.secrets = {
@@ -16,39 +20,18 @@ in {
 
   programs.opencode = {
     enable = true;
+    package = pkgs.writeShellScriptBin "opencode" ''
+      export ANTHROPIC_API_KEY="$(cat ${config.sops.secrets.anthropic_api_key.path})"
+      export OPENAI_API_KEY="$(cat ${config.sops.secrets.openrouter_api_key.path})"
+      export OPENROUTER_API_KEY="$(cat ${config.sops.secrets.openai_api_key.path})"
+      exec ${pkgs.opencode}/bin/opencode "$@"
+    '';
+
     settings = {
       "$schema" = "https://opencode.ai/config.json";
       theme = "gruvbox";
-      model = "deepseek/deepseek-r1-0528:free";
       autoshare = false;
       autoupdate = true;
-      provider = {
-        openai = {
-          apiKey = "{file:${config.sops.secrets.openai_api_key.path}}";
-          models = {
-            "gpt-4.1-2025-04-14".name = "GPT 4.1";
-            "o4-mini-2025-04-16".name = "GPT o4 Mini";
-            "gpt-4o-2024-11-20".name = "GPT 4o";
-          };
-        };
-        anthropic = {
-          apiKey = "{file:${config.sops.secrets.anthropic_api_key.path}}";
-          models = {
-            "claude-sonnet-4-20250514".name = "Claude 4 Sonnet";
-            "claude-3-5-sonnet-latest".name = "Claude 3.5 Sonnet";
-          };
-        };
-        openrouter = {
-          apiKey = "{file:${config.sops.secrets.openrouter_api_key.path}}";
-          models = {
-            "google/gemini-2.5-flash".name = "Gemni 2.5 Flash";
-            "google/gemini-2.5-flash-preview-05-20:thinking".name = "Gemni 2.5 Flash (Thinking) Preview";
-            "google/gemini-2.5-pro".name = "Gemni 2.5 Pro";
-            "deepseek/deepseek-r1-0528:free".name = "Deepseek R1 0528 (Free)";
-            "deepseek/deepseek-r1:free".name = "Deepseek R1 (Free)";
-          };
-        };
-      };
       mcp = {
         "nixos" = {
           type = "local";
@@ -56,7 +39,7 @@ in {
           command = [
             "nix"
             "run"
-            "github=utensils/mcp-nixos"
+            "github:utensils/mcp-nixos"
             "--"
           ];
         };
