0e759c2af51b0dee12f3a92e4e88b49861101719
zshen <zshen@adobe.com>
2025-07-16 10:12:49 -0600

    [nvf] add format toggle

--- packages/nvf/config/lua/keymaps.lua
+++ packages/nvf/config/lua/keymaps.lua
@@ -13,3 +13,13 @@ vim.keymap.set("x", "p", [["_dP]])
 
 -- diagnostic
 map("<leader>e", vim.diagnostic.open_float, "Show diagnostic [E]rror messages")
+
+map("<leader>tf", function()
+  if vim.b.disable_autoformat or vim.g.disable_autoformat then
+    vim.cmd("FormatEnable")
+    vim.notify("AutoFormt Enabled")
+  else
+    vim.cmd("FormatDisable")
+    vim.notify("AutoFormt Disabled")
+  end
+end, "Toggle [F]ormat")
