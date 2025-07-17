68f0c910b56cf04e44d8dccf088f820774266788
zshen <zshen@adobe.com>
2025-07-17 15:34:25 -0600

    [zsh] change adobe script target

---
+++ modules/shared/home-manager/features/zsh/adobe.sh
@@ -0,0 +1,7 @@
+age-encryption.org/v1
+-> X25519 QyCs/SkTyRSSn8RVitLDr/MrLfY6hsIiUVD0eEAvGk8
+bEW5HyI+f/ZEM9E4xm7CBohykqGvIuW4yV8yzh98hf0
+-> x`jW`TO~-grease
+hubylC6h9g
+--- wohSWQhuMRNxiDXDmoCI8NyydtY0uzruUo0fMiCF9yo
+Q®9Œ'¦l­1Hè¢ûU7b$µ@¹^ü­çÇž¦µÆÍ>?‘,ô30’‡-X£;¹[òÏS‚ß_óæÚTbáºèƒ)à|”Ð‚R5Bn[pÔÞs„Mcf±Ê½j,m5Éø1—Ì³à%!‰#u%‚d“‰RëˆpÃütUäe¼'º«:L–7»ã¹Ùàè£\#«²\©)ÔTvùÚ™Ò¹oƒø©GpSövÂ‚MfCôÃy–®W]9kÖøM}Z¢„»Þ„iÉš“|”'¯Õ9…]iÜùK)„5ÑÄ
--- modules/shared/home-manager/features/zsh/default.nix
+++ modules/shared/home-manager/features/zsh/default.nix
@@ -5,7 +5,6 @@
   currentSystemName,
   ...
 }: let
-  initExtraAdobe = ./adobe.nix;
 in {
   home.file = {
     ".local/share/zsh/zsh-autosuggestions".source = "${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions";
@@ -42,8 +41,9 @@ in {
 
           ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#${config.lib.stylix.colors.base03},bg=cyan,bold,underline"
           source "$HOME/.local/share/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh"
-        ''
-        ++ initExtraAdobe;
+
+          source "${./adobe.sh}"
+        '';
 
       # Load before fzf to resolve conflicting shortcuts
       initExtraBeforeCompInit =
