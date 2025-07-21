a8384a1fd6785e96a024d0761234095e6acd4849
zshen <zshen@adobe.com>
2025-07-21 12:05:11 -0600

    [zsh] add adobe.nix

---
+++ modules/shared/home-manager/features/zsh/adobe.nix
@@ -0,0 +1,8 @@
+age-encryption.org/v1
+-> X25519 vY0JtPIvMeDg3Rs9QO320y3kvYz3fS2v1pPfka1MIFw
+L1Gz3xuPenhOwqyEvAczpYqDsKzJQk5EzPXXb2E5ag8
+-> ZNs3JO:-grease 9zi
+TgWaT7JVq8XCoWR5eEbePt40aNn64dMwmQaQ4bMwBGIxbUR7EGFgvQAWMlxBsTk4
+ADrMLsRfVx4GIuCr
+--- 50eqc6qqBEtJguJ1UQ1ZcbMMpspWtGqYLSpNE9LNl4c
+―όd„΅φ&5Mω½>'ΩςL7l9°~έ,‘(g―ΰ΅΄Òεάνµ3N\Γ&2=Ajuf©ϋwΖ,αή
