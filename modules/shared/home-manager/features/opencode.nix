9b5e2be72e5d031cfec9f0dca2400ac5882967a4
zshen <zshen@adobe.com>
2025-07-30 12:09:49 -0700

    [opencode] fix

--- modules/shared/home-manager/features/opencode.nix
+++ modules/shared/home-manager/features/opencode.nix
@@ -32,31 +32,37 @@ in {
       theme = "gruvbox";
       model = "deepseek/deepseek-r1-0528:free";
       autoshare = false;
-      autoupdate = true;
+      autoupdate = false;
       provider = {
         openai = {
-          api_key = "{file:${config.sops.secrets.openai_api_key.path}}";
-          models = {
-            "gpt-4.1-2025-04-14".name = "GPT 4.1";
-            "o4-mini-2025-04-16".name = "GPT o4 Mini";
-            "gpt-4o-2024-11-20".name = "GPT 4o";
+          options = {
+            apiKey = "{file:${config.sops.secrets.openai_api_key.path}}";
+            models = {
+              "gpt-4.1-2025-04-14".name = "GPT 4.1";
+              "o4-mini-2025-04-16".name = "GPT o4 Mini";
+              "gpt-4o-2024-11-20".name = "GPT 4o";
+            };
           };
         };
         anthropic = {
-          api_key = "{file:${config.sops.secrets.anthropic_api_key.path}}";
-          models = {
-            "claude-sonnet-4-20250514".name = "Claude 4 Sonnet";
-            "claude-3-5-sonnet-latest".name = "Claude 3.5 Sonnet";
+          options = {
+            apiKey = "{file:${config.sops.secrets.anthropic_api_key.path}}";
+            models = {
+              "claude-sonnet-4-20250514".name = "Claude 4 Sonnet";
+              "claude-3-5-sonnet-latest".name = "Claude 3.5 Sonnet";
+            };
           };
         };
         openrouter = {
-          api_key = "{file:${config.sops.secrets.openrouter_api_key.path}}";
-          models = {
-            "google/gemini-2.5-flash".name = "Gemni 2.5 Flash";
-            "google/gemini-2.5-flash-preview-05-20:thinking".name = "Gemni 2.5 Flash (Thinking) Preview";
-            "google/gemini-2.5-pro".name = "Gemni 2.5 Pro";
-            "deepseek/deepseek-r1-0528:free".name = "Deepseek R1 0528 (Free)";
-            "deepseek/deepseek-r1:free".name = "Deepseek R1 (Free)";
+          options = {
+            apiKey = "{file:${config.sops.secrets.openrouter_api_key.path}}";
+            models = {
+              "google/gemini-2.5-flash".name = "Gemni 2.5 Flash";
+              "google/gemini-2.5-flash-preview-05-20:thinking".name = "Gemni 2.5 Flash (Thinking) Preview";
+              "google/gemini-2.5-pro".name = "Gemni 2.5 Pro";
+              "deepseek/deepseek-r1-0528:free".name = "Deepseek R1 0528 (Free)";
+              "deepseek/deepseek-r1:free".name = "Deepseek R1 (Free)";
+            };
           };
         };
       };
