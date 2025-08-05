24dfc7359a521aa12ca6d396adfd43d543a1c6c4
zshen <zhongjie.x.shen@gmail.com>
2025-08-05 14:31:47 -0600

    [zen] hyprland config

--- modules/nixos/home-manager/features/hyprland/default.nix
+++ modules/nixos/home-manager/features/hyprland/default.nix
@@ -70,7 +70,7 @@ in {
         windowrule = workspace name:gaming,class:^(gamescope)$
 
         windowrule = workspace name:zen,class:^(Vivaldi-stable)$
-        windowrule = workspace name:zen,class:^(zen)$
+        windowrule = workspace name:zen,class:^(zen-beta)$
 
         # Pavucontrol floating
         windowrule = float,class:(.*org.pulseaudio.pavucontrol.*)
