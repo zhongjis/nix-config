d4196a7768fe3b786fc74049597d530df750c782
zshen <zhongjie.x.shen@gmail.com>
2025-08-15 12:56:57 -0600

    [git] update config

--- modules/shared/home-manager/features/git.nix
+++ modules/shared/home-manager/features/git.nix
@@ -11,10 +11,69 @@
     userEmail = lib.mkDefault "zhongjie.x.shen@gmail.com";
 
     aliases = {};
+    attributes = [];
 
-    attributes = [
-      "push.default current"
-    ];
+    # TODO: fancy signature for commits and tags
+    # signing = {
+    #   format = "ssh";
+    # };
+
+    diff-so-fancy = {
+      enable = true;
+      markEmptyLines = false;
+    };
+
+    extraConfig = {
+      core = {
+        compression = 9;
+        whitespace = "error";
+        preloadindex = true;
+      };
+      init = {
+        defaultBranch = "main";
+      };
+      status = {
+        branch = true;
+        showStash = true;
+        showUntrackedFiles = "all";
+      };
+      diff = {
+        context = 3;
+        renames = "copies";
+        interHunkContext = 10;
+      };
+      push = {
+        autoSetupRemote = true;
+        default = "current";
+        followTags = true;
+      };
+      pull = {
+        default = "current";
+        rebase = true;
+      };
+      rebase = {
+        autoStash = true;
+        missingCommitsCheck = true;
+      };
+      branch.sort = "-committerdate";
+      tag.sort = "-taggerdate";
+    };
+  };
+
+  programs.zsh.shellAliases = {
+    gs = "git status --short";
+
+    ga = "git add";
+    gc = "git commit";
+
+    gp = "git push";
+    gu = "git pull"; # git update
+
+    gl = "git log";
+    glo = "git log --oneline";
+    gb = "git branch";
+
+    gd = "git diff";
   };
 
   home.packages = with pkgs; [
