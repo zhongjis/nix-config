a0c9e1fd2f4735215732c8a0d19def723b626e4c
zshen <zhongjie.x.shen@gmail.com>
2025-08-15 17:19:54 -0600

    [hyprland] add hypr-dynamic-cursors plugin

--- modules/nixos/home-manager/features/hyprland/plugins.nix
+++ modules/nixos/home-manager/features/hyprland/plugins.nix
@@ -10,8 +10,16 @@ in {
     plugins = with pkgs; [
       hyprlandPlugins.hy3
       hyprlandPlugins.csgo-vulkan-fix
+      hyprlandPlugins.hypr-dynamic-cursors
     ];
     settings = {
+      "plugin:dynamic-cursors" = {
+        mode = "none";
+        shake = {
+          threadhold = 10.0;
+          limit = 4.0;
+        };
+      };
     };
   };
 }
