f9520593763045aed8eed930374a5f8d69236b0c
zshen <zshen@adobe.com>
2025-08-15 11:19:05 -0600

    [flake] remove adobe.sh

--- modules/shared/home-manager/features/zsh/default.nix
+++ modules/shared/home-manager/features/zsh/default.nix
@@ -40,8 +40,6 @@ in {
 
           ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#${config.lib.stylix.colors.base03},bg=cyan,bold,underline"
           source "$HOME/.local/share/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh"
-
-          source "${./adobe.sh}"
         '';
 
       # Load before fzf to resolve conflicting shortcuts
