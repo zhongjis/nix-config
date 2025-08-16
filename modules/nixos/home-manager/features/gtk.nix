2b935a3a9cde2ace146f2ebee58df8a722af590e
zshen <zhongjie.x.shen@gmail.com>
2025-08-15 18:14:17 -0600

    [gtk] udpate

--- modules/nixos/home-manager/features/gtk.nix
+++ modules/nixos/home-manager/features/gtk.nix
@@ -4,8 +4,8 @@
   ...
 }: {
   gtk.enable = true;
-  gtk.iconTheme.name = "Dracula";
-  gtk.iconTheme.package = pkgs.dracula-icon-theme;
-  gtk.theme.name = lib.mkForce "Dracula";
-  gtk.theme.package = lib.mkForce pkgs.dracula-theme;
+  # gtk.iconTheme.name = "Dracula";
+  # gtk.iconTheme.package = pkgs.dracula-icon-theme;
+  # gtk.theme.name = lib.mkForce "Dracula";
+  # gtk.theme.package = lib.mkForce pkgs.dracula-theme;
 }
