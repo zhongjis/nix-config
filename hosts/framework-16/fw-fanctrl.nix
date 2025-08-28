9b8cf1affe1f327d73f8df577d68ae8c4e0f1cd5
zshen <zhongjie.x.shen@gmail.com>
2025-08-28 17:18:51 -0600

    [fw-fanctrl] reenable fw-fanctrl

--- hosts/framework-16/fw-fanctrl.nix
+++ hosts/framework-16/fw-fanctrl.nix
@@ -1,39 +1,43 @@
-{inputs, ...}: {
-  imports = [inputs.fw-fanctrl.nixosModules.default];
-
-  hardware.fw-fanctrl.enable = true;
-  hardware.fw-fanctrl.config = {
-    defaultStrategy = "lazy";
-    strategies = {
-      "lazy" = {
-        fanSpeedUpdateFrequency = 5;
-        movingAverageInterval = 30;
-        speedCurve = [
-          {
-            temp = 0;
-            speed = 15;
-          }
-          {
-            temp = 50;
-            speed = 15;
-          }
-          {
-            temp = 65;
-            speed = 25;
-          }
-          {
-            temp = 70;
-            speed = 35;
-          }
-          {
-            temp = 80;
-            speed = 50;
-          }
-          {
-            temp = 85;
-            speed = 100;
-          }
-        ];
+{pkgs, ...}: {
+  hardware.fw-fanctrl = {
+    enable = true;
+    # TODO: wait https://github.com/TamtamHero/fw-fanctrl/issues/159 to be fixed
+    package = pkgs.fw-fanctrl.overrideAttrs (finalAttrs: prevAttrs: {
+      patches = (prevAttrs.patches or []) ++ [./fw-fanctrl.patch];
+    });
+    config = {
+      defaultStrategy = "lazy";
+      strategies = {
+        "lazy" = {
+          fanSpeedUpdateFrequency = 5;
+          movingAverageInterval = 30;
+          speedCurve = [
+            {
+              temp = 0;
+              speed = 15;
+            }
+            {
+              temp = 50;
+              speed = 15;
+            }
+            {
+              temp = 65;
+              speed = 25;
+            }
+            {
+              temp = 70;
+              speed = 35;
+            }
+            {
+              temp = 80;
+              speed = 50;
+            }
+            {
+              temp = 85;
+              speed = 100;
+            }
+          ];
+        };
       };
     };
   };
