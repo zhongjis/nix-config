e9518ed7f98dccc03e39c498108487da74cc499c
zshen <zshen@adobe.com>
2025-09-08 11:36:41 -0600

    [zen-browser] disable cust

--- modules/shared/home-manager/features/zen-browser.nix
+++ modules/shared/home-manager/features/zen-browser.nix
@@ -10,33 +10,33 @@
 
   programs.zen-browser = {
     enable = true;
-    policies = let
-      locked = value: {
-        Value = value;
-        Status = "locked";
-      };
-    in {
-      AutofillAddressEnabled = true;
-      AutofillCreditCardEnabled = false;
-      DisableAppUpdate = true;
-      DisableFeedbackCommands = true;
-      DisableFirefoxStudies = true;
-      DisablePocket = true; # save webs for later reading
-      DisableTelemetry = true;
-      DontCheckDefaultBrowser = true;
-      NoDefaultBookmarks = true;
-      OfferToSaveLogins = false;
-      EnableTrackingProtection = {
-        Value = true;
-        Locked = true;
-        Cryptomining = true;
-        Fingerprinting = true;
-      };
-      Preferences = builtins.mapAttrs (_: locked) {
-        "browser.tabs.warnOnClose" = false;
-        "media.videocontrols.picture-in-picture.video-toggle.enabled" = true;
-      };
-    };
+    # policies = let
+    #   locked = value: {
+    #     Value = value;
+    #     Status = "locked";
+    #   };
+    # in {
+    #   AutofillAddressEnabled = true;
+    #   AutofillCreditCardEnabled = false;
+    #   DisableAppUpdate = true;
+    #   DisableFeedbackCommands = true;
+    #   DisableFirefoxStudies = true;
+    #   DisablePocket = true; # save webs for later reading
+    #   DisableTelemetry = true;
+    #   DontCheckDefaultBrowser = true;
+    #   NoDefaultBookmarks = true;
+    #   OfferToSaveLogins = false;
+    #   EnableTrackingProtection = {
+    #     Value = true;
+    #     Locked = true;
+    #     Cryptomining = true;
+    #     Fingerprinting = true;
+    #   };
+    #   Preferences = builtins.mapAttrs (_: locked) {
+    #     "browser.tabs.warnOnClose" = false;
+    #     "media.videocontrols.picture-in-picture.video-toggle.enabled" = true;
+    #   };
+    # };
   };
 
   # NOTE: for some reason this only works under stylix.nix under hm module
