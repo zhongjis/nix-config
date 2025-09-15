3ff192f33465e540b2db8c71a88c09e757f83caa
zshen <zshen@adobe.com>
2025-09-15 14:42:56 -0700

    [postman] replaced by insomnia

--- modules/darwin/bundles/work.nix
+++ modules/darwin/bundles/work.nix
@@ -69,7 +69,7 @@ in {
       "sublime-merge@dev"
       "cursor"
       "dbeaver-community"
-      "postman"
+      "insomnia"
     ];
   };
 }
--- modules/nixos/bundles/developer.nix
+++ modules/nixos/bundles/developer.nix
@@ -24,7 +24,7 @@
       mongodb-compass
       dbeaver-bin
 
-      postman
+      insomnia
     ]
     ++ [
     ];
