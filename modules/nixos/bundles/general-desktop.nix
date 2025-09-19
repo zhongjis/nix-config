c72362dd30f6ca4ecb91640a08562f0295f90319
zshen <zhongjie.x.shen@gmail.com>
2025-09-19 16:42:32 -0700

    [os] update timezone

--- modules/nixos/bundles/general-desktop.nix
+++ modules/nixos/bundles/general-desktop.nix
@@ -41,7 +41,7 @@
   };
 
   # Time Zone
-  time.timeZone = "America/Denver";
+  time.timeZone = "America/Los_Angeles";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
