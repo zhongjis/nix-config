f0887b534d2b2b284cf0e9050d323a67573ffb3e
zshen <zshen@adobe.com>
2025-10-22 09:51:17 -0700

    [feat] add rg

--- modules/shared/home-manager/features/rg.nix
+++ modules/shared/home-manager/features/rg.nix
@@ -1,5 +1,20 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [
-    ripgrep
-  ];
+  programs.ripgrep = {
+    enable = true;
+    arguments = [
+      "--max-columns-preview"
+      "--colors=line:style:bold"
+      "--hidden"
+      "--smart-case"
+      "--follow"
+      "--glob '!node_modules/*'"
+      "--glob '!.direnv/*'"
+      "--glob '!.git/*'"
+      "--color=always"
+      "--line-number"
+      "--column"
+      "--sort=path"
+      "--max-columns=200"
+    ];
+  };
 }
