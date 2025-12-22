3ac4527220906d5e535141c75e9872a624f2cdb1
zshen <zhongjie.x.shen@gmail.com>
2025-12-21 22:52:28 -0800

    flake: update

--- modules/shared/default.nix
+++ modules/shared/default.nix
@@ -22,6 +22,7 @@
       "https://mitchellh-nixos-config.cachix.org"
       "https://chaotic-nyx.cachix.org/"
       "https://zshen-cache-default.cachix.org"
+      "https://cache.flakehub.com"
     ];
     trusted-public-keys = [
       "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg="
@@ -33,6 +34,7 @@
       "mitchellh-nixos-config.cachix.org-1:bjEbXJyLrL1HZZHBbO4QALnI5faYZppzkU4D2s0G8RQ="
       "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
       "zshen-cache-default.cachix.org-1:+UMXDbrtkRBEOF6NU4F1S8lKvjC6bxyB0llevRggXo4="
+      "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM= cache.flakehub.com-4:Asi8qIv291s0aYLyH6IOnr5Kf6+OF14WVjkE6t3xMio= cache.flakehub.com-5:zB96CRlL7tiPtzA9/WKyPkp3A2vqxqgdgyTVNGShPDU= cache.flakehub.com-6:W4EGFwAGgBj3he7c5fNh9NkOXw0PUVaxygCVKeuvaqU= cache.flakehub.com-7:mvxJ2DZVHn/kRxlIaxYNMuDG1OvMckZu32um1TadOR8= cache.flakehub.com-8:moO+OVS0mnTjBTcOUh2kYLQEd59ExzyoW1QgQ8XAARQ= cache.flakehub.com-9:wChaSeTI6TeCuV/Sg2513ZIM9i0qJaYsF+lZCXg0J6o= cache.flakehub.com-10:2GqeNlIp6AKp4EF2MVbE1kBOp9iBSyo0UPR9KoR0o1Y="
     ];
   };
 
