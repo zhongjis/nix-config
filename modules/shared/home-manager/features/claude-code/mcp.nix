bb8fe2556768e46affd3a2663eda488650b4341c
zshen <zhongjie.x.shen@gmail.com>
2025-12-26 14:35:41 -0800

    feat: claude code update

--- modules/shared/home-manager/features/claude-code/mcp.nix
+++ modules/shared/home-manager/features/claude-code/mcp.nix
@@ -1,18 +1,38 @@
 {
   lib,
   pkgs,
+  config,
   ...
 }: {
+  sops.secrets = {
+    # github - personal
+    n8n_api_key = {
+      sopsFile = ../../../../../secrets/homelab.yaml;
+    };
+    github_com_zhongjis_mcp = {
+      sopsFile = ../../../../../secrets/access-tokens.yaml;
+    };
+  };
+
   home.packages = [pkgs.nodejs];
   programs.claude-code.mcpServers = {
     github = {
       type = "stdio";
-      command = lib.getExe pkgs.github-mcp-server;
+      command = "docker";
       args = [
-        # NOTE: avoid accidentally causing unexpected changes with default MCP and whitelist allow
-        "--read-only"
-        "stdio"
+        "run"
+        "-i"
+        "--rm"
+        "-e"
+        "GITHUB_PERSONAL_ACCESS_TOKEN"
+        "-e"
+        "GITHUB_HOST"
+        "ghcr.io/github/github-mcp-server"
       ];
+      env = {
+        GITHUB_HOST = "https://github.com";
+        GITHUB_PERSONAL_ACCESS_TOKEN = "$(cat ${config.sops.secrets.github_com_zhongjis_mcp.path})";
+      };
     };
     nixos = {
       type = "stdio";
@@ -35,6 +55,31 @@
     socket = {
       type = "http";
       url = "https://mcp.socket.dev/";
+    };
+    mcp_n8n = {
+      type = "stdio";
+      command = "docker";
+      args = [
+        "run"
+        "-i"
+        "--rm"
+        "--init"
+        "-e"
+        "MCP_MODE=stdio"
+        "-e"
+        "LOG_LEVEL=error"
+        "-e"
+        "DISABLE_CONSOLE_OUTPUT=true"
+        "-e"
+        "N8N_API_URL"
+        "-e"
+        "N8N_API_KEY"
+        "ghcr.io/czlonkowski/n8n-mcp:latest"
+      ];
+      env = {
+        N8N_API_URL = "https://n8n.zshen.me";
+        N8N_API_KEY = "$(cat ${config.sops.secrets.n8n_api_key.path})";
+      };
     };
   };
 }
