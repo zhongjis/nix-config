e1e75abdb42ca304219aa4d025eb33fa7d674451
zshen <zshen@adobe.com>
2026-01-02 12:33:41 -0800

    mcp: tweak

--- modules/shared/home-manager/features/mcp.nix
+++ modules/shared/home-manager/features/mcp.nix
@@ -3,27 +3,18 @@
   pkgs,
   ...
 }: {
-  sops.secrets = {
-    # github - personal
-    n8n_api_key = {
-      sopsFile = ../../../../../secrets/homelab.yaml;
-    };
-    github_com_zhongjis_mcp = {
-      sopsFile = ../../../../../secrets/access-tokens.yaml;
-    };
-  };
   programs.mcp = {
     enable = true;
     servers = {
-      "nixos" = {
+      nixos = {
         command = "nix";
         args = ["run" "github:utensils/mcp-nixos"];
       };
-      "mcp_k8s" = {
+      mcp_k8s = {
         command = "nix";
         args = ["run" "nixpkgs#mcp-k8s-go"];
       };
-      "flux-operator-mcp" = {
+      flux_operator_mcp = {
         command = "nix";
         args = ["run" "nixpkgs#fluxcd-operator-mcp" "serve"];
         environment = {
@@ -34,31 +25,6 @@
         type = "http";
         url = "https://mcp.socket.dev/";
       };
-      # mcp_n8n = {
-      #   type = "stdio";
-      #   command = "docker";
-      #   args = [
-      #     "run"
-      #     "-i"
-      #     "--rm"
-      #     "--init"
-      #     "-e"
-      #     "MCP_MODE=stdio"
-      #     "-e"
-      #     "LOG_LEVEL=error"
-      #     "-e"
-      #     "DISABLE_CONSOLE_OUTPUT=true"
-      #     "-e"
-      #     "N8N_API_URL"
-      #     "-e"
-      #     "N8N_API_KEY"
-      #     "ghcr.io/czlonkowski/n8n-mcp:latest"
-      #   ];
-      #   env = {
-      #     N8N_API_URL = "https://n8n.zshen.me";
-      #     N8N_API_KEY = "$(cat ${config.sops.secrets.n8n_api_key.path})";
-      #   };
-      # };
     };
   };
 }
