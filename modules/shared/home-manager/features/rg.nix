21d8f84b43843c2e4feb4172fd1d308f7482a75d
zshen <zhongjie.x.shen@gmail.com>
2026-01-02 01:42:26 -0800

    rg: tweak

--- modules/shared/home-manager/features/rg.nix
+++ modules/shared/home-manager/features/rg.nix
@@ -4,16 +4,10 @@
     arguments = [
       "--max-columns-preview"
       "--colors=line:style:bold"
-      "--hidden"
       "--smart-case"
       "--follow"
-      "--glob '!node_modules/*'"
-      "--glob '!.direnv/*'"
-      "--glob '!.git/*'"
-      "--color=always"
       "--line-number"
       "--column"
-      "--sort=path"
       "--max-columns=200"
     ];
   };
