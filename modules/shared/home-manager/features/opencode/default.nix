66647ea70a072017271678d77d7d923cfd226739
zshen <zshen@adobe.com>
2026-01-02 11:43:42 -0800

    opencode: remove comemnted code

--- modules/shared/home-manager/features/opencode/default.nix
+++ modules/shared/home-manager/features/opencode/default.nix
@@ -13,12 +13,6 @@
   ];
   programs.opencode = {
     enable = true;
-    # package = pkgs.writeShellScriptBin "opencode" ''
-    #   export ANTHROPIC_API_KEY="$(cat ${config.sops.secrets.anthropic_api_key.path})"
-    #   export OPENAI_API_KEY="$(cat ${config.sops.secrets.openrouter_api_key.path})"
-    #   export OPENROUTER_API_KEY="$(cat ${config.sops.secrets.openai_api_key.path})"
-    #   exec ${pkgs.opencode}/bin/opencode "$@"
-    # '';
     enableMcpIntegration = true;
 
     settings = {
