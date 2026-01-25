ce4894ef48e7d1aa08e6979a23d07e65a4672046
zhongjis <zhongjie.x.shen@gmail.com>
2026-01-24 17:20:25 -0800

    opencode: update

--- modules/home-manager/features/opencode/plugins/oh-my-opencode/default.nix
+++ modules/home-manager/features/opencode/plugins/oh-my-opencode/default.nix
@@ -1,12 +1,12 @@
 _: {
   programs.opencode.settings = {
     plugin = [
-      "oh-my-opencode@latest"
+      "oh-my-opencode@v3.0.0"
     ];
   };
 
-  xdg.configFile = {
-    # "opencode/oh-my-opencode.json".source = ../oh-my-opencode/cheap-but-not-free.json;
-    "opencode/oh-my-opencode.json".source = ./v3-poorman-pretends-rich.json;
-  };
+  # xdg.configFile = {
+  #   # "opencode/oh-my-opencode.json".source = ../oh-my-opencode/cheap-but-not-free.json;
+  #   "opencode/oh-my-opencode.json".source = ./v3-poorman-pretends-rich.json;
+  # };
 }
