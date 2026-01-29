848509a3941ef932c012e10233994f0fbb69d131
zshen <zhongjie.x.shen@gmail.com>
2026-01-28 18:38:45 -0800

    feat: add nextjs template and some other stuffs

--- templates/nextjs/flake.nix
+++ templates/nextjs/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "NodeJS dev env";
+  description = "T3 Stack dev env";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
@@ -21,13 +21,24 @@
         pkgs = nixpkgs.legacyPackages.${system};
       in {
         devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            prisma-engines
+            prisma
+          ];
           shell = "/bin/zsh";
           shellHook = ''
-            echo "[[nodejs_22]] shell activated!!!"
+            export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig";
+            export PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
+            export PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine"
+            export PRISMA_QUERY_ENGINE_LIBRARY="${pkgs.prisma-engines}/lib/libquery_engine.node"
+            export PRISMA_FMT_BINARY="${pkgs.prisma-engines}/bin/prisma-fmt"
+            echo "[[NextJS]] shell activated!!!"
           '';
           packages = with pkgs; [
             nodejs_22
             pnpm
+            openssl
+            sqlite
           ];
         };
       }
