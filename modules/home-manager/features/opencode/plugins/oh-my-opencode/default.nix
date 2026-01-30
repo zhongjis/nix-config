db8ea3b0a2a012ab74bb9bbf70158dde1f90b4a4
zshen <zshen@adobe.com>
2026-01-30 12:16:10 -0800

    omo: conditional config

--- modules/home-manager/features/opencode/plugins/oh-my-opencode/default.nix
+++ modules/home-manager/features/opencode/plugins/oh-my-opencode/default.nix
@@ -1,10 +1,16 @@
 {
   lib,
   hasPlugin,
+  currentSystemName,
   ...
-}:
-lib.mkIf (hasPlugin "oh-my-opencode") {
-  xdg.configFile = {
-    "opencode/oh-my-opencode.jsonc".source = ./copilot-work.jsonc;
-  };
-}
+}: let
+  configFile =
+    if currentSystemName == "mac-m1-max"
+    then ./copilot-work.jsonc
+    else ./copilot-personal.jsonc;
+in
+  lib.mkIf (hasPlugin "oh-my-opencode") {
+    xdg.configFile = {
+      "opencode/oh-my-opencode.jsonc".source = configFile;
+    };
+  }
