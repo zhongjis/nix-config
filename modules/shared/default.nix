ba33ebbff75bcf734bff42fe507e397b4cf85b6c
zhongjis <zhongjie.x.shen@gmail.com>
2026-02-05 19:10:19 -0800

    flake: update

--- modules/shared/default.nix
+++ modules/shared/default.nix
@@ -14,6 +14,8 @@
   # caches
   nix.settings = {
     trusted-users = ["root" "@wheel" "zshen"];
+    # Prefer caches over local builds
+    builders-use-substitutes = true;
     substituters = [
       "https://cache.nixos.org"
       "https://colmena.cachix.org"
@@ -27,6 +29,7 @@
       "https://cache.flakehub.com"
     ];
     trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg="
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
