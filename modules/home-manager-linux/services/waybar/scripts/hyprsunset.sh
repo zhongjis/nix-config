f89c904b63a41177f94a222b4e7bd3ca912215c1
zshen <zhongjie.x.shen@gmail.com>
2026-02-06 19:55:04 -0800

    fix(waybar): use fgcolor for Pango markup in hyprsunset script
    - Replace 'color=' with 'fgcolor=' for proper Pango span foreground color
    - Simplify quote escaping using JSON backslash notation

--- modules/home-manager-linux/services/waybar/scripts/hyprsunset.sh
+++ modules/home-manager-linux/services/waybar/scripts/hyprsunset.sh
@@ -6,11 +6,11 @@ get_status() {
   if systemctl --user is-active --quiet hyprsunset.service; then
     # Service is running (blue light filter active)
     # Color #7da389 matches the CSS text color for active state
-    echo '{"text": "<span color='"'"'#202020'"'"' bgcolor='"'"'#7da389'"'"' > 󰖨 </span> ON", "tooltip": "Hyprsunset: Active (click to disable)", "class": "active"}'
+    echo '{"text": "<span fgcolor=\"#202020\" bgcolor=\"#7da389\"> 󰖨 </span> ON", "tooltip": "Hyprsunset: Active (click to disable)", "class": "active"}'
   else
     # Service is stopped (no filter)
     # Color #ea6962 (red) matches the CSS text color for inactive state (same as muted)
-    echo '{"text": "<span color='"'"'#202020'"'"' bgcolor='"'"'#ea6962'"'"' > 󰖨 </span> OFF", "tooltip": "Hyprsunset: Inactive (click to enable)", "class": "inactive"}'
+    echo '{"text": "<span fgcolor=\"#202020\" bgcolor=\"#ea6962\"> 󰖨 </span> OFF", "tooltip": "Hyprsunset: Inactive (click to enable)", "class": "inactive"}'
   fi
 }
 
