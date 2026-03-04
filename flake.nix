39279e38200ca8d8ee573d99d70f52ca855a1233
zshen <zshen@adobe.com>
2026-03-04 14:37:07 -0800

    update opencode

--- flake.nix
+++ flake.nix
@@ -68,7 +68,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };
-    opencode.url = "github:anomalyco/opencode/v1.2.16";
+    opencode.url = "github:anomalyco/opencode/v1.2.17";
 
     ## hyprland
     hyprland.url = "github:hyprwm/Hyprland/v0.54.1";
