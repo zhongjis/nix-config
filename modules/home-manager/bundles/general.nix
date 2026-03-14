7c0ad5ab053869052c5e827e8bc4c4bc0b39726a
zhongjis <zhongjie.x.shen@gmail.com>
2026-03-13 18:42:42 -0700

    fix syntax

--- modules/home-manager/bundles/general.nix
+++ modules/home-manager/bundles/general.nix
@@ -1,7 +1,6 @@
 {
   pkgs,
   config,
-  inputs,
   lib,
   inputs,
   ...
@@ -43,14 +42,12 @@
 
   home.packages = [
     inputs.self.packages.${pkgs.stdenv.hostPlatform.system}.openkanban
+    (lib.optional
+      (inputs.self.packages.${pkgs.stdenv.hostPlatform.system} ? agent-of-empires)
+      inputs.self.packages.${pkgs.stdenv.hostPlatform.system}.agent-of-empires)
   ];
 
   home.sessionVariables = {
     FLAKE = "${config.home.homeDirectory}/personal/nix-config";
   };
-
-  home.packages =
-    lib.optional
-    (inputs.self.packages.${pkgs.stdenv.hostPlatform.system} ? agent-of-empires)
-    inputs.self.packages.${pkgs.stdenv.hostPlatform.system}.agent-of-empires;
 }
