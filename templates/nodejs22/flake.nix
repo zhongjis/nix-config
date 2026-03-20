f515c7d6c380a2ed69eea363daa035f4ae559557
zhongjis <zhongjie.x.shen@gmail.com>
2026-03-19 20:40:10 -0700

    templates(nodejs22): add numtide cache config

--- templates/nodejs22/flake.nix
+++ templates/nodejs22/flake.nix
@@ -1,4 +1,9 @@
 {
+  nixConfig = {
+    extra-substituters = ["https://cache.numtide.com"];
+    extra-trusted-public-keys = ["niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="];
+  };
+
   description = "NodeJS dev env";
 
   inputs = {
