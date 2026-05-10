cf5666a18bbfcb5481b0741f2992f4feda4376ab
zshen <zhongjie.x.shen@gmail.com>
2026-05-09 21:36:23 -0700

    fix typo

--- modules/home-manager/features/obsidian.nix
+++ modules/home-manager/features/obsidian.nix
@@ -1,4 +1,14 @@
-{...}: {
+{
+  pkgs,
+  inputs,
+  ...
+}: let
+  inherit (pkgs.stdenv.hostPlatform) system;
+  llmAgentsPackages = inputs.llm-agents.packages.${system};
+in {
+  home.packages = [
+    llmAgentsPackages.qmd
+  ];
   # NOTE: package managed outside Home Manager (NixOS system packages / Homebrew cask)
   programs.obsidian = {
     enable = true;
