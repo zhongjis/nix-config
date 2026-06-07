bbe71086e6856daaf44bb98de6ca2a45c2cf146b
zshen <zhongjie.x.shen@gmail.com>
2026-06-07 16:48:44 -0700

    postgresql oci

--- modules/nixos/services/postgresql.nix
+++ modules/nixos/services/postgresql.nix
@@ -1,13 +1,26 @@
-{
-  lib,
-  pkgs,
-  ...
-}: {
-  services.postgresql = {
-    enable = true;
-    package = pkgs.postgresql_17;
-    authentication = lib.mkForce ''
-      local all all trust
-    '';
+{lib, ...}: {
+  services.postgresql.enable = lib.mkForce false;
+  virtualisation.oci-containers.containers.postgresql = {
+    image = "supabase/postgres:17.6.1.084";
+    autoStart = true;
+    ports = [
+      "127.0.0.1:5432:5432"
+    ];
+    volumes = [
+      "/var/lib/supabase-postgres/data:/var/lib/postgresql/data"
+    ];
+    environment = {
+      POSTGRES_USER = "postgres";
+      POSTGRES_PASSWORD = "postgres";
+      POSTGRES_DB = "postgres";
+      PGDATA = "/var/lib/postgresql/data";
+    };
+    cmd = [
+      "postgres"
+      "-c"
+      "config_file=/etc/postgresql/postgresql.conf"
+      "-c"
+      "log_min_messages=fatal"
+    ];
   };
 }
