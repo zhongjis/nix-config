cfe12a087c478f06a7895f6e8d237ac0315901dc
zshen <zhongjie.x.shen@gmail.com>
2026-06-14 00:13:10 -0700

    bump stable nixpkgs version

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-26.05";
     nixos-hardware.url = "github:nixos/nixos-hardware/master";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
