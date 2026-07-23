73d05105669c77b3a8f65cd59f768de5e69b9be5
zhongjis <23202735+zhongjis@users.noreply.github.com>
2026-07-23 10:08:01 +0000

    chore(opencode-morph-fast-apply): update package

--- packages/opencode-morph-fast-apply.nix
+++ packages/opencode-morph-fast-apply.nix
@@ -5,13 +5,13 @@
 }:
 bun2nix.mkDerivation {
   pname = "opencode-morph-fast-apply";
-  version = "1.10.0";
+  version = "1.10.2";
 
   src = pkgs.fetchFromGitHub {
     owner = "JRedeker";
     repo = "opencode-morph-fast-apply";
-    rev = "7481cb0d8fe939bae0595dce422497161196ccac";
-    hash = "sha256-tz4T03Zw5HJUwLeRf58qjsVr+LKXauY1C8QWJFm9+rI=";
+    rev = "0ab744099ab2a2a2b5a5628c7cf1275e673b640b";
+    hash = "sha256-y84bdfjTRGlSMXAOa+KIzDSQw6vim2t0hw8wx1pFPkg=";
   };
 
   bunDeps = bun2nix.fetchBunDeps {
