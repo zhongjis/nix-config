56b9123c6480b4cfea830fb7a19cb1ac548e2980
zshen <zhongjie.x.shen@gmail.com>
2026-07-24 01:49:01 -0700

    pi: update hashline edit version

--- modules/home-manager/features/ai-tools/pi/default.nix
+++ modules/home-manager/features/ai-tools/pi/default.nix
@@ -161,7 +161,7 @@
     # Resources
     packages = [
       "git:github.com/mavam/pi-mcporter@v0.5.0"
-      "git:github.com/RimuruW/pi-hashline-edit@v0.7.0"
+      "git:github.com/RimuruW/pi-hashline-edit@v0.8.3"
       "git:github.com/aliou/pi-guardrails@v0.15.0"
       "git:github.com/davebcn87/pi-autoresearch@v1.6.0"
       "git:github.com/nicobailon/pi-web-access@v0.13.0"
