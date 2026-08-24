0ba6c1f101e85d7f7e8e3e362f6aaa723be77e24
zshen <zshen@adobe.com>
2026-08-24 14:20:02 -0700

    feat: update web access for pi

--- modules/home-manager/features/ai-tools/pi/default.nix
+++ modules/home-manager/features/ai-tools/pi/default.nix
@@ -169,7 +169,7 @@
       "git:github.com/mavam/pi-mcporter@v1.0.2"
       "git:github.com/jesseylin/pi-hashline-edit@v0.8.3"
       # "git:github.com/davebcn87/pi-autoresearch@v1.6.2"
-      "git:github.com/nicobailon/pi-web-access@v0.23.0"
+      "git:github.com/nicobailon/pi-web-access@v0.24.2"
       "git:github.com/nicobailon/pi-interactive-shell@v0.15.0"
       "git:github.com/aliou/pi-processes@v0.10.9"
       "git:github.com/dbachelder/pi-btw@v0.4.1"
