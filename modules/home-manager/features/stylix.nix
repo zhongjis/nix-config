6b7c2bd6759411138a76887d9b2ac25b3819f28a
zshen <zhongjie.x.shen@gmail.com>
2026-09-08 18:42:16 -0700

    fix: migrate Stylix lualine theme option

--- modules/home-manager/features/stylix.nix
+++ modules/home-manager/features/stylix.nix
@@ -1,10 +1,46 @@
 {
   config,
   lib,
+  inputs,
+  pkgs,
   ...
-}: {
+}: let
+  neovimModule = inputs.stylix + "/modules/neovim/hm.nix";
+  nvfModule = inputs.stylix + "/modules/neovim/nvf.nix";
+  mkTarget = import (inputs.stylix + "/stylix/mk-target.nix") {
+    humanName = (import (inputs.stylix + "/stylix/meta.nix") {inherit lib pkgs;}).neovim.name;
+    name = "neovim";
+  };
+  # Temporarily migrate Stylix's lualine option until upstream supports the new nvf path.
+  patchedNvfModule = builtins.toFile "stylix-nvf.nix" (builtins.replaceStrings
+    ["lualine.theme"]
+    ["lualine.setupOpts.options.theme"]
+    (builtins.readFile nvfModule));
+in {
   imports = [
     ../../shared/stylix_common.nix
+    {
+      disabledModules = [neovimModule];
+      imports = [
+        (import neovimModule {
+          inherit mkTarget;
+          lib =
+            lib
+            // {
+              modules =
+                lib.modules
+                // {
+                  importApply = module:
+                    lib.modules.importApply (
+                      if module == nvfModule
+                      then patchedNvfModule
+                      else module
+                    );
+                };
+            };
+        })
+      ];
+    }
   ];
 
   gtk.gtk4.theme = lib.mkForce config.gtk.theme;
